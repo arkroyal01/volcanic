arkroyal01/volcanic

commit d97bf9b1f47cf45365997cb19d5b27b3e3795126

Check for EGL_KHR_platform_x11 in addition to EGL_EXT_platform_x11
Summary:
Some platforms (like Mali) only report the newer EGL_KHR_platform_x11
on X11.

Reviewers: kwin

Subscribers: kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D2933

plugins/platforms/x11/common/eglonxbackend.cpp
@@ -181,8 +181,10 @@ bool EglOnXBackend::initRenderingContext()
         setHavePlatformBase(havePlatformBase);
         if (havePlatformBase) {
             // Make sure that the X11 platform is supported
-            if (!hasClientExtension(QByteArrayLiteral("EGL_EXT_platform_x11"))) {
-                qCWarning(KWIN_CORE) << "EGL_EXT_platform_base is supported, but EGL_EXT_platform_x11 is not. Cannot create EGLDisplay on X11";
+            if (!hasClientExtension(QByteArrayLiteral("EGL_EXT_platform_x11")) &&
+                !hasClientExtension(QByteArrayLiteral("EGL_KHR_platform_x11")) {
+                qCWarning(KWIN_CORE) << "EGL_EXT_platform_base is supported, but neither EGL_EXT_platform_x11 nor EGL_KHR_platform_x11 is supported."
+                                     << "Cannot create EGLDisplay on X11";
                 return false;
             }
 

commit ce6673e7dfcb1b2925580fe787a1b2294d77701f

Fix the build
d97bf9b1f47cf45365997cb19d5b27b3e3795126 broke the build due to a
missing bracket.

plugins/platforms/x11/common/eglonxbackend.cpp
@@ -182,7 +182,7 @@ bool EglOnXBackend::initRenderingContext()
         if (havePlatformBase) {
             // Make sure that the X11 platform is supported
             if (!hasClientExtension(QByteArrayLiteral("EGL_EXT_platform_x11")) &&
-                !hasClientExtension(QByteArrayLiteral("EGL_KHR_platform_x11")) {
+                !hasClientExtension(QByteArrayLiteral("EGL_KHR_platform_x11"))) {
                 qCWarning(KWIN_CORE) << "EGL_EXT_platform_base is supported, but neither EGL_EXT_platform_x11 nor EGL_KHR_platform_x11 is supported."
                                      << "Cannot create EGLDisplay on X11";
                 return false;