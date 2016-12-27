arkroyal01/volcanic

commit adcc7d8b21b3802b9378aa531b400ac90072e763

Place OSD windows again on size changes
Summary:
The position of an OSD is based on the size of the window. If the
size of the OSD changed, it would not be positioned correctly any more.

With this change the OSD window gets placed again if the size changes.
Thus it's also placed correctly even if the window changed it's size.

Reviewers: #kwin, #plasma

Subscribers: plasma-devel, kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D3720

abstract_client.cpp
@@ -69,6 +69,18 @@ AbstractClient::AbstractClient()
     connect(this, &AbstractClient::paletteChanged, this, &AbstractClient::triggerDecorationRepaint);
 
     connect(Decoration::DecorationBridge::self(), &QObject::destroyed, this, &AbstractClient::destroyDecoration);
+
+    // replace on-screen-display on size changes
+    connect(this, &AbstractClient::geometryShapeChanged, this,
+        [this] (Toplevel *c, const QRect &old) {
+            Q_UNUSED(c)
+            if (isOnScreenDisplay() && !geometry().isEmpty() && old.size() != geometry().size()) {
+                QRect area = workspace()->clientArea(PlacementArea, Screens::self()->current(), desktop());
+                Placement::self()->place(this, area);
+                setGeometryRestore(geometry());
+            }
+        }
+    );
 }
 
 AbstractClient::~AbstractClient()

autotests/integration/plasma_surface_test.cpp
@@ -244,6 +244,13 @@ void PlasmaSurfaceTest::testOSDPlacement()
     QCOMPARE(screens()->geometry(1), geometries.at(1));
 
     QCOMPARE(c->geometry(), QRect(590, 649, 100, 50));
+
+    // change size of window
+    QSignalSpy geometryChangedSpy(c, &AbstractClient::geometryShapeChanged);
+    QVERIFY(geometryChangedSpy.isValid());
+    Test::render(surface.data(), QSize(200, 100), Qt::red);
+    QVERIFY(geometryChangedSpy.wait());
+    QCOMPARE(c->geometry(), QRect(540, 616, 200, 100));
 }
 
 void PlasmaSurfaceTest::testPanelTypeHasStrut_data()

commit 21b024f01305944ba59c85f059fec434253b10bf

Fix the osd re-placement on X11
Summary:
adcc7d8b21b3802b9378aa531b400ac90072e763 introduced a regression hitting
an assert on X11 when an osd window is added.

This change tries to address this by adding a GeometryUpdateBlocker
around the change. The hit assertion is:

    pendingGeometryUpdate() == PendingGeometryNone ||
    areGeometryUpdatesBlocked()

By using a GeometryUpdateBlocker the second condition becomes true
and we no longer hit the condition.

BUG: 374038

Test Plan:
Manual tested both on X11 and Wayland, Wayland autotest
still passes

Reviewers: #kwin, #plasma

Subscribers: plasma-devel, kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D3808

abstract_client.cpp
@@ -75,6 +75,7 @@ AbstractClient::AbstractClient()
         [this] (Toplevel *c, const QRect &old) {
             Q_UNUSED(c)
             if (isOnScreenDisplay() && !geometry().isEmpty() && old.size() != geometry().size()) {
+                GeometryUpdatesBlocker blocker(this);
                 QRect area = workspace()->clientArea(PlacementArea, Screens::self()->current(), desktop());
                 Placement::self()->place(this, area);
                 setGeometryRestore(geometry());