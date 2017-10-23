arkroyal01/volcanic

commit c80962d03cfe0e693cdad7d8b73d9f9ec788214e

Add physicalSize to the Screens API
Summary:
Allows to share the implementation in a better way and is a requirement
to get the Screen implementation in the QPA plugin to be based on
KWin::Screens instead of KWayland::Output.

Reviewers: #kwin, #plasma

Subscribers: plasma-devel, kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D8344

plugins/platforms/hwcomposer/hwcomposer_backend.cpp
@@ -194,8 +194,9 @@ static KWayland::Server::OutputInterface *createOutput(hwc_composer_device_1_t *
 
     if (attr_values[2] != 0 && attr_values[3] != 0) {
          static const qreal factor = 25.4;
-         o->setPhysicalSize(QSizeF(qreal(pixel.width() * 1000) / qreal(attr_values[2]) * factor,
-                                   qreal(pixel.height() * 1000) / qreal(attr_values[3]) * factor).toSize());
+         m_physicalSize = QSizeF(qreal(pixel.width() * 1000) / qreal(attr_values[2]) * factor,
+                                 qreal(pixel.height() * 1000) / qreal(attr_values[3]) * factor);
+         o->setPhysicalSize(m_physicalSize.toSize());
     } else {
          // couldn't read physical size, assume 96 dpi
          o->setPhysicalSize(pixel / 3.8);

plugins/platforms/hwcomposer/hwcomposer_backend.h
@@ -85,6 +85,9 @@ public:
     QVector<CompositingType> supportedCompositors() const override {
         return QVector<CompositingType>{OpenGLCompositing};
     }
+    QSizeF physicalSize() const {
+        return m_physicalSize;
+    }
 
 Q_SIGNALS:
     void outputBlankChanged();
@@ -110,6 +113,7 @@ private:
     QMutex m_vsyncMutex;
     QWaitCondition m_vsyncWaitCondition;
     QScopedPointer<BacklightInputEventFilter> m_filter;
+    QSizeF m_physicalSize;
 };
 
 class HwcomposerWindow : public HWComposerNativeWindow

plugins/platforms/drm/drm_output.cpp
@@ -243,6 +243,7 @@ bool DrmOutput::init(drmModeConnector *connector)
         qCWarning(KWIN_DRM) << "Overwriting monitor physical size for" << m_edid.eisaId << "/" << m_edid.monitorName << "/" << m_edid.serialNumber << " from " << physicalSize << "to " << overwriteSize;
         physicalSize = overwriteSize;
     }
+    m_physicalSize = physicalSize;
     m_waylandOutput->setPhysicalSize(physicalSize);
     m_waylandOutputDevice->setPhysicalSize(physicalSize);
 

plugins/platforms/drm/drm_output.h
@@ -103,6 +103,10 @@ public:
         return m_uuid;
     }
 
+    QSize physicalSize() const {
+        return m_physicalSize;
+    }
+
 Q_SIGNALS:
     void dpmsChanged();
 
@@ -159,6 +163,7 @@ private:
     bool m_pageFlipPending = false;
     bool m_dpmsAtomicOffPending = false;
     bool m_modesetRequested = true;
+    QSize m_physicalSize;
 };
 
 }

plugins/platforms/drm/screens_drm.cpp
@@ -113,4 +113,13 @@ float DrmScreens::refreshRate(int screen) const
     return outputs.at(screen)->currentRefreshRate() / 1000.0f;
 }
 
+QSizeF DrmScreens::physicalSize(int screen) const
+{
+    const auto outputs = m_backend->outputs();
+    if (screen >= outputs.size()) {
+        return Screens::physicalSize(screen);
+    }
+    return outputs.at(screen)->physicalSize();
+}
+
 }

plugins/platforms/drm/screens_drm.h
@@ -40,6 +40,8 @@ public:
     QString name(int screen) const override;
     float refreshRate(int screen) const override;
 
+    QSizeF physicalSize(int screen) const override;
+
 private:
     DrmBackend *m_backend;
 };

plugins/platforms/hwcomposer/screens_hwcomposer.cpp
@@ -37,4 +37,13 @@ float HwcomposerScreens::refreshRate(int screen) const
     return m_backend->refreshRate() / 1000.0f;
 }
 
+QSizeF HwcomposerScreens::physicalSize(int screen) const
+{
+    const QSizeF size = m_backend->physicalSize();
+    if (size.isValid()) {
+        return size;
+    }
+    return Screens::physicalSize(screen);
+}
+
 }

plugins/platforms/hwcomposer/screens_hwcomposer.h
@@ -32,6 +32,7 @@ public:
     HwcomposerScreens(HwcomposerBackend *backend, QObject *parent = nullptr);
     virtual ~HwcomposerScreens();
     float refreshRate(int screen) const override;
+    QSizeF physicalSize(int screen) const override;
 
 private:
     HwcomposerBackend *m_backend;

commit 2bbca3dcf517fb2833949cea0f694d4360169979

[platforms/hwcomposer] Make createOutput a non-static method
Summary:
there is no need for createOutput to be static, make it non-static,
this also fixes the build failure introduced in commit
c80962d03cfe0e693cdad7d8b73d9f9ec788214e.

Test Plan: builds

Reviewers: #kwin, graesslin, davidedmundson

Reviewed By: davidedmundson

Subscribers: kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D8430

plugins/platforms/hwcomposer/hwcomposer_backend.cpp
@@ -27,14 +27,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "wayland_server.h"
 // KWayland
 #include <KWayland/Server/display.h>
-#include <KWayland/Server/output_interface.h>
 #include <KWayland/Server/seat_interface.h>
 // Qt
 #include <QKeyEvent>
 #include <QDBusConnection>
 // hybris/android
 #include <hardware/hardware.h>
-#include <hardware/hwcomposer.h>
 #include <hardware/lights.h>
 // linux
 #include <linux/input.h>
@@ -164,7 +162,7 @@ HwcomposerBackend::~HwcomposerBackend()
     }
 }
 
-static KWayland::Server::OutputInterface *createOutput(hwc_composer_device_1_t *device)
+KWayland::Server::OutputInterface* HwcomposerBackend::createOutput(hwc_composer_device_1_t *device)
 {
     uint32_t configs[5];
     size_t numConfigs = 5;

plugins/platforms/hwcomposer/hwcomposer_backend.h
@@ -28,9 +28,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <android-config.h>
 // libhybris
+#include <hardware/hwcomposer.h>
 #include <hwcomposer_window.h>
 // needed as hwcomposer_window.h includes EGL which on non-arm includes Xlib
 #include <fixx11h.h>
+
+#include <KWayland/Server/output_interface.h>
 
 typedef struct hwc_display_contents_1 hwc_display_contents_1_t;
 typedef struct hwc_layer_1 hwc_layer_1_t;
@@ -101,6 +104,7 @@ private Q_SLOTS:
 private:
     void initLights();
     void toggleScreenBrightness();
+    KWayland::Server::OutputInterface* createOutput(hwc_composer_device_1_t *device);
     QSize m_displaySize;
     hwc_composer_device_1_t *m_device = nullptr;
     light_device_t *m_lights = nullptr;