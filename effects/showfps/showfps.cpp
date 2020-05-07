arkroyal01/volcanic

commit 6dc13a795dfcd244bb135fd995aea8ef5370e303

Fix compiler warnings
Summary: No need to keep them around for no reason.

Test Plan: Tested the plugins I thought could be affected. Have been using it for a couple of days without problems

Reviewers: #kwin, zzag

Reviewed By: #kwin, zzag

Subscribers: zzag, kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D28062

effects/showfps/showfps.cpp
@@ -123,11 +123,7 @@ void ShowFpsEffect::reconfigure(ReconfigureFlags)
 
 void ShowFpsEffect::prePaintScreen(ScreenPrePaintData& data, int time)
 {
-    if (time == 0) {
-        // TODO optimized away
-    }
-    t.start();
-    frames[ frames_pos ] = t.minute() * 60000 + t.second() * 1000 + t.msec();
+    frames[ frames_pos ] = t.restart();
     if (++frames_pos == MAX_FPS)
         frames_pos = 0;
     effects->prePaintScreen(data, time);
@@ -158,7 +154,7 @@ void ShowFpsEffect::paintScreen(int mask, const QRegion &region, ScreenPaintData
     for (int i = 0;
             i < MAX_FPS;
             ++i)
-        if (abs(t.minute() * 60000 + t.second() * 1000 + t.msec() - frames[ i ]) < 1000)
+        if (abs(t.elapsed() - frames[ i ]) < 1000)
             ++fps; // count all frames in the last second
     if (fps > MAX_TIME)
         fps = MAX_TIME; // keep it the same height

effects/showfps/showfps.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef KWIN_SHOWFPS_H
 #define KWIN_SHOWFPS_H
 
-#include <QTime>
+#include <QElapsedTimer>
 #include <QFont>
 
 #include <kwineffects.h>
@@ -83,7 +83,7 @@ private:
     void paintDrawSizeGraph(int x, int y);
     void paintGraph(int x, int y, QList<int> values, QList<int> lines, bool colorize);
     QImage fpsTextImage(int fps);
-    QTime t;
+    QElapsedTimer t;
     enum { NUM_PAINTS = 100 }; // remember time needed to paint this many paints
     int paints[ NUM_PAINTS ]; // time needed to paint
     int paint_size[ NUM_PAINTS ]; // number of pixels painted

CMakeLists.txt
@@ -4,7 +4,7 @@ project(KWin)
 set(PROJECT_VERSION "5.18.80")
 set(PROJECT_VERSION_MAJOR 5)
 
-set(QT_MIN_VERSION "5.12.0")
+set(QT_MIN_VERSION "5.14.0")
 set(KF5_MIN_VERSION "5.66.0")
 
 set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules ${CMAKE_MODULE_PATH})

abstract_wayland_output.cpp
@@ -196,7 +196,7 @@ void AbstractWaylandOutput::applyChanges(const KWayland::Server::OutputChangeSet
         overallSizeCheckNeeded = true;
     }
     if (changeSet->scaleChanged()) {
-        qCDebug(KWIN_CORE) << "Setting scale:" << changeSet->scale();
+        qCDebug(KWIN_CORE) << "Setting scale:" << changeSet->scaleF();
         setScale(changeSet->scaleF());
         emitModeChanged = true;
     }

debug_console.cpp
@@ -709,7 +709,7 @@ DebugConsoleDelegate::~DebugConsoleDelegate() = default;
 
 QString DebugConsoleDelegate::displayText(const QVariant &value, const QLocale &locale) const
 {
-    switch (value.type()) {
+    switch (value.userType()) {
     case QMetaType::QPoint: {
         const QPoint p = value.toPoint();
         return QStringLiteral("%1,%2").arg(p.x()).arg(p.y());

decorations/settings.cpp
@@ -56,9 +56,7 @@ SettingsImpl::SettingsImpl(KDecoration2::DecorationSettings *parent)
     );
     // prevent changes in Decoration due to Compositor being destroyed
     connect(Compositor::self(), &Compositor::aboutToDestroy, this,
-        [this, c] {
-            disconnect(c);
-        }
+        [c] { disconnect(c); }
     );
     connect(Workspace::self(), &Workspace::configChanged, this, &SettingsImpl::readSettings);
     connect(DecorationBridge::self(), &DecorationBridge::metaDataLoaded, this, &SettingsImpl::readSettings);

effects/highlightwindow/highlightwindow.cpp
@@ -61,7 +61,7 @@ void HighlightWindowEffect::prePaintWindow(EffectWindow* w, WindowPrePaintData& 
     if (!m_highlightedWindows.isEmpty()) {
         // Initial fade out and changing highlight animation
         if (opacity == m_windowOpacity.end())
-            opacity = m_windowOpacity.insertMulti(w, 0.0f);
+            opacity = m_windowOpacity.insert(w, 0.0f);
         float oldOpacity = *opacity;
         if (m_highlightedWindows.contains(w))
             *opacity = qMin(1.0f, oldOpacity + time / m_fadeDuration);
@@ -250,7 +250,7 @@ void HighlightWindowEffect::prepareHighlighting()
     m_finishing = false;
     foreach (EffectWindow * w, effects->stackingOrder()) {
         if (!m_windowOpacity.contains(w))    // Just in case we are still finishing from last time
-            m_windowOpacity.insertMulti(w, isInitiallyHidden(w) ? 0.0 : 1.0);
+            m_windowOpacity.insert(w, isInitiallyHidden(w) ? 0.0 : 1.0);
         if (!m_highlightedWindows.isEmpty())
             m_highlightedWindows.at(0)->addRepaintFull();
     }

effects/screenshot/screenshot.cpp
@@ -658,7 +658,7 @@ void ScreenShotEffect::convertFromGLImage(QImage &img, int w, int h)
     // see https://github.com/qt/qtbase/blob/dev/src/opengl/qgl.cpp
     if (QSysInfo::ByteOrder == QSysInfo::BigEndian) {
         // OpenGL gives RGBA; Qt wants ARGB
-        uint *p = (uint*)img.bits();
+        uint *p = reinterpret_cast<uint *>(img.bits());
         uint *end = p + w * h;
         while (p < end) {
             uint a = *p << 24;
@@ -668,7 +668,7 @@ void ScreenShotEffect::convertFromGLImage(QImage &img, int w, int h)
     } else {
         // OpenGL gives ABGR (i.e. RGBA backwards); Qt wants ARGB
         for (int y = 0; y < h; y++) {
-            uint *q = (uint*)img.scanLine(y);
+            uint *q = reinterpret_cast<uint *>(img.scanLine(y));
             for (int x = 0; x < w; ++x) {
                 const uint pixel = *q;
                 *q = ((pixel << 16) & 0xff0000) | ((pixel >> 16) & 0xff)

commit b3af64ac51d78773a6768cb117c0bd00b0aa2b01

[effects/showfps] Calculate "correct" FPS
Summary:
6dc13a795dfcd2 broke code that calculates the value of the fps counter.

The frames field stores timestamps of previous frames, not time between
each two consecutive frames.

This change doesn't attempt to make the show fps compute more accurate
performance metrics, e.g. how much times it takes to execute rendering
commands on the GPU, etc.

Test Plan: The Show FPS effect displays more sensible values.

Reviewers: #kwin, davidedmundson

Reviewed By: #kwin, davidedmundson

Subscribers: davidedmundson, broulik, kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D29247

effects/showfps/showfps.cpp
@@ -123,13 +123,14 @@ void ShowFpsEffect::reconfigure(ReconfigureFlags)
 
 void ShowFpsEffect::prePaintScreen(ScreenPrePaintData& data, int time)
 {
-    frames[ frames_pos ] = t.restart();
+    frames[ frames_pos ] = QDateTime::currentMSecsSinceEpoch();
     if (++frames_pos == MAX_FPS)
         frames_pos = 0;
     effects->prePaintScreen(data, time);
     data.paint += fps_rect;
 
     paint_size[ paints_pos ] = 0;
+    t.restart();
 }
 
 void ShowFpsEffect::paintWindow(EffectWindow* w, int mask, QRegion region, WindowPaintData& data)
@@ -150,11 +151,15 @@ void ShowFpsEffect::paintWindow(EffectWindow* w, int mask, QRegion region, Windo
 void ShowFpsEffect::paintScreen(int mask, const QRegion &region, ScreenPaintData& data)
 {
     effects->paintScreen(mask, region, data);
+    int lastFrame = frames_pos - 1;
+    if (lastFrame < 0)
+        lastFrame = MAX_FPS - 1;
+    const qint64 lastTimestamp = frames[lastFrame];
     int fps = 0;
     for (int i = 0;
             i < MAX_FPS;
             ++i)
-        if (abs(t.elapsed() - frames[ i ]) < 1000)
+        if (abs(lastTimestamp - frames[ i ]) < 1000)
             ++fps; // count all frames in the last second
     if (fps > MAX_TIME)
         fps = MAX_TIME; // keep it the same height

effects/showfps/showfps.h
@@ -89,7 +89,7 @@ private:
     int paint_size[ NUM_PAINTS ]; // number of pixels painted
     int paints_pos;  // position in the queue
     enum { MAX_FPS = 200 };
-    int frames[ MAX_FPS ]; // (sec*1000+msec) of the time the frame was done
+    qint64 frames[ MAX_FPS ]; // the time when the frame was done
     int frames_pos; // position in the queue
     double alpha;
     int x;