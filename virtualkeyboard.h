SocioProphet/kwin

Integrate input-method-unstable-v1

virtualkeyboard.h
@@ -57,10 +57,12 @@ private:
     void setEnabled(bool enable);
     void updateSni();
     void updateInputPanelState();
+    void adoptInputMethodContext();
 
     bool m_enabled = false;
     KStatusNotifierItem *m_sni = nullptr;
     QScopedPointer<QQuickView> m_inputWindow;
+    QPointer<AbstractClient> m_inputSurface;
     QPointer<AbstractClient> m_trackedClient;
     // If a surface loses focus immediately after being resized by the keyboard, don't react to it to avoid resize loops
     QTimer *m_floodTimer;

wayland_server.cpp
@@ -22,10 +22,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "platform.h"
 #include "composite.h"
 #include "idle_inhibition.h"
+#include "inputpanelclient.h"
 #include "screens.h"
 #include "waylandxdgshellintegration.h"
 #include "workspace.h"
 #include "xdgshellclient.h"
+#include "deleted.h"
 #include "service_utils.h"
 
 // Client
@@ -67,6 +69,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <KWaylandServer/keystate_interface.h>
 #include <KWaylandServer/filtered_display.h>
 #include <KWaylandServer/keyboard_shortcuts_inhibit_v1_interface.h>
+#include <KWaylandServer/inputmethod_v1_interface.h>
 
 // Qt
 #include <QCryptographicHash>
@@ -203,6 +206,13 @@ void WaylandServer::registerXdgGenericClient(AbstractClient *client)
         return;
     }
     qCDebug(KWIN_CORE) << "Received invalid xdg client:" << client->surface();
+}
+
+AbstractClient *WaylandServer::createInputPanelClient(KWaylandServer::InputPanelSurfaceV1Interface *surface)
+{
+    auto *client = new InputPanelV1Client(surface);
+    registerShellClient(client);
+    return client;
 }
 
 class KWinDisplay : public KWaylandServer::FilteredDisplay
@@ -453,6 +463,8 @@ bool WaylandServer::init(const QByteArray &socketName, InitializationFlags flags
 
     m_keyState = m_display->createKeyStateInterface(m_display);
     m_keyState->create();
+
+    m_inputMethod = m_display->createInputMethodInterface(m_display);
 
     return true;
 }

arkroyal01/volcanic

virtualkeyboard.h
@@ -46,10 +46,12 @@ private:
     void setEnabled(bool enable);
     void updateSni();
     void updateInputPanelState();
+    void adoptInputMethodContext();
 
     bool m_enabled = false;
     KStatusNotifierItem *m_sni = nullptr;
     QScopedPointer<QQuickView> m_inputWindow;
+    QPointer<AbstractClient> m_inputClient;
     QPointer<AbstractClient> m_trackedClient;
     // If a surface loses focus immediately after being resized by the keyboard, don't react to it to avoid resize loops
     QTimer *m_floodTimer;

wayland_server.cpp
@@ -12,6 +12,7 @@
 #include "platform.h"
 #include "composite.h"
 #include "idle_inhibition.h"
+#include "inputpanelv1client.h"
 #include "screens.h"
 #include "waylandxdgshellintegration.h"
 #include "workspace.h"
@@ -57,6 +58,7 @@
 #include <KWaylandServer/keystate_interface.h>
 #include <KWaylandServer/filtered_display.h>
 #include <KWaylandServer/keyboard_shortcuts_inhibit_v1_interface.h>
+#include <KWaylandServer/inputmethod_v1_interface.h>
 
 // Qt
 #include <QCryptographicHash>
@@ -223,6 +225,13 @@ AbstractWaylandOutput *WaylandServer::findOutput(KWaylandServer::OutputInterface
         }
     }
     return outputFound;
+}
+
+AbstractClient *WaylandServer::createInputPanelClient(KWaylandServer::InputPanelSurfaceV1Interface *surface)
+{
+    auto *client = new InputPanelV1Client(surface);
+    registerShellClient(client);
+    return client;
 }
 
 class KWinDisplay : public KWaylandServer::FilteredDisplay
@@ -474,6 +483,8 @@ bool WaylandServer::init(const QByteArray &socketName, InitializationFlags flags
 
     m_keyState = m_display->createKeyStateInterface(m_display);
     m_keyState->create();
+
+    m_inputMethod = m_display->createInputMethodInterface(m_display);
 
     return true;
 }