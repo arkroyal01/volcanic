do-mybest/kwin

Introduce RenderLoop
At the moment, our frame scheduling infrastructure is still heavily
based on Xinerama-style rendering. Specifically, we assume that painting
is driven by a single timer, etc.

This change introduces a new type - RenderLoop. Its main purpose is to
drive compositing on a specific output, or in case of X11, on the
overlay window.

With RenderLoop, compositing is synchronized to vblank events. It
exposes the last and the next estimated presentation timestamp. The
expected presentation timestamp can be used by effects to ensure that
animations are synchronized with the upcoming vblank event.

On Wayland, every outputs has its own render loop. On X11, per screen
rendering is not possible, therefore the platform exposes the render
loop for the overlay window. Ideally, the Scene has to expose the
RenderLoop, but as the first step towards better compositing scheduling
it's good as is for the time being.

The RenderLoop tries to minimize the latency by delaying compositing as
close as possible to the next vblank event. One tricky thing about it is
that if compositing is too close to the next vblank event, animations
may become a little bit choppy. However, increasing the latency reduces
the choppiness.

Given that, there is no any "silver bullet" solution for the choppiness
issue, a new option has been added in the Compositing KCM to specify the
amount of latency. By default, it's "Medium," but if a user is not
satisfied with the upstream default, they can tweak it.

plugins/platforms/drm/drm_backend.cpp
@@ -16,6 +16,7 @@
 #include "logging.h"
 #include "logind.h"
 #include "main.h"
+#include "renderloop_p.h"
 #include "scene_qpainter_drm_backend.h"
 #include "screens_drm.h"
 #include "udev.h"
@@ -189,8 +190,11 @@ void DrmBackend::reactivate()
     }
     // restart compositor
     m_pageFlipsPending = 0;
+
+    for (DrmOutput *output : qAsConst(m_outputs)) {
+        output->renderLoop()->uninhibit();
+    }
     if (Compositor *compositor = Compositor::self()) {
-        compositor->bufferSwapComplete();
         compositor->addRepaintFull();
     }
 }
@@ -200,36 +204,61 @@ void DrmBackend::deactivate()
     if (!m_active) {
         return;
     }
-    // block compositor
-    if (m_pageFlipsPending == 0 && Compositor::self()) {
-        Compositor::self()->aboutToSwapBuffers();
+
+    for (DrmOutput *output : qAsConst(m_outputs)) {
+        output->hideCursor();
+        output->renderLoop()->inhibit();
     }
-    // hide cursor and disable
-    for (auto it = m_outputs.constBegin(); it != m_outputs.constEnd(); ++it) {
-        DrmOutput *o = *it;
-        o->hideCursor();
-    }
+
     m_active = false;
+}
+
+static std::chrono::nanoseconds convertTimestamp(const timespec &timestamp)
+{
+    return std::chrono::seconds(timestamp.tv_sec) + std::chrono::nanoseconds(timestamp.tv_nsec);
+}
+
+static std::chrono::nanoseconds convertTimestamp(clockid_t sourceClock, clockid_t targetClock,
+                                                 const timespec &timestamp)
+{
+    if (sourceClock == targetClock) {
+        return convertTimestamp(timestamp);
+    }
+
+    timespec sourceCurrentTime = {};
+    timespec targetCurrentTime = {};
+
+    clock_gettime(sourceClock, &sourceCurrentTime);
+    clock_gettime(targetClock, &targetCurrentTime);
+
+    const auto delta = convertTimestamp(sourceCurrentTime) - convertTimestamp(timestamp);
+    return convertTimestamp(targetCurrentTime) - delta;
 }
 
 void DrmBackend::pageFlipHandler(int fd, unsigned int frame, unsigned int sec, unsigned int usec, void *data)
 {
     Q_UNUSED(fd)
     Q_UNUSED(frame)
-    Q_UNUSED(sec)
-    Q_UNUSED(usec)
-    auto output = reinterpret_cast<DrmOutput*>(data);
+
+    auto output = static_cast<DrmOutput *>(data);
+
+    DrmGpu *device = output->gpu();
+    DrmBackend *backend = output->m_backend;
+
+    std::chrono::nanoseconds timestamp = convertTimestamp(device->timestampClock(),
+                                                          CLOCK_MONOTONIC,
+                                                          { sec, usec * 1000 });
+    if (timestamp == std::chrono::nanoseconds::zero()) {
+        qCDebug(KWIN_DRM, "Got invalid timestamp (sec: %u, usec: %u) on output %s",
+                sec, usec, qPrintable(output->name()));
+        timestamp = std::chrono::steady_clock::now().time_since_epoch();
+    }
 
     output->pageFlipped();
-    output->m_backend->m_pageFlipsPending--;
-    if (output->m_backend->m_pageFlipsPending == 0) {
-        // TODO: improve, this currently means we wait for all page flips or all outputs.
-        // It would be better to driver the repaint per output
+    backend->m_pageFlipsPending--;
 
-        if (Compositor::self()) {
-            Compositor::self()->bufferSwapComplete();
-        }
-    }
+    RenderLoopPrivate *renderLoopPrivate = RenderLoopPrivate::get(output->renderLoop());
+    renderLoopPrivate->notifyFrameCompleted(timestamp);
 }
 
 void DrmBackend::openDrm()
@@ -503,9 +532,6 @@ bool DrmBackend::present(DrmBuffer *buffer, DrmOutput *output)
 
     if (output->present(buffer)) {
         m_pageFlipsPending++;
-        if (m_pageFlipsPending == 1 && Compositor::self()) {
-            Compositor::self()->aboutToSwapBuffers();
-        }
         return true;
     } else if (output->gpu()->deleteBufferAfterPageFlip()) {
         delete buffer;

plugins/platforms/virtual/scene_qpainter_virtual_backend.cpp
@@ -7,9 +7,11 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "scene_qpainter_virtual_backend.h"
-#include "virtual_backend.h"
 #include "cursor.h"
 #include "screens.h"
+#include "softwarevsyncmonitor.h"
+#include "virtual_backend.h"
+#include "virtual_output.h"
 
 #include <QPainter>
 
@@ -38,7 +40,8 @@ bool VirtualQPainterBackend::needsFullRepaint(int screenId) const
 
 void VirtualQPainterBackend::beginFrame(int screenId)
 {
-    Q_UNUSED(screenId)
+    VirtualOutput *output = static_cast<VirtualOutput *>(m_backend->findOutput(screenId));
+    output->vsyncMonitor()->start();
 }
 
 void VirtualQPainterBackend::createOutputs()

plugins/platforms/x11/standalone/sgivideosyncvsyncmonitor.h
@@ -1,0 +1,76 @@
+/*
+    SPDX-FileCopyrightText: 2020 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "vsyncmonitor.h"
+
+#include <epoxy/glx.h>
+#include <GL/glxext.h>
+#include "fixx11h.h"
+
+#include <QThread>
+
+namespace KWin
+{
+
+/**
+ * The SGIVideoSyncVsyncMonitorHelper class is responsible for waiting for vsync events on the
+ * root window. Note that the helper runs on a separate thread.
+ */
+class SGIVideoSyncVsyncMonitorHelper : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit SGIVideoSyncVsyncMonitorHelper(QObject *parent = nullptr);
+    ~SGIVideoSyncVsyncMonitorHelper() override;
+
+    bool isValid() const;
+
+public Q_SLOTS:
+    void poll();
+
+Q_SIGNALS:
+    void errorOccurred();
+    void vblankOccurred(std::chrono::nanoseconds timestamp);
+
+private:
+    Display *m_display = nullptr;
+    GLXContext m_localContext = 0;
+    GLXDrawable m_drawable = 0;
+};
+
+/**
+ * The SGIVideoSyncVsyncMonitor class monitors vblank events using the GLX_SGI_video_sync
+ * extension.
+ *
+ * Vblank events are monitored in a separated thread to avoid blocking the main thread. In
+ * order to avoid locking up the main X11 connection, the worker thread establishes its own
+ * X11 connection.
+ */
+class SGIVideoSyncVsyncMonitor : public VsyncMonitor
+{
+    Q_OBJECT
+
+public:
+    static SGIVideoSyncVsyncMonitor *create(QObject *parent);
+
+    ~SGIVideoSyncVsyncMonitor() override;
+
+    bool isValid() const;
+
+public Q_SLOTS:
+    void start() override;
+
+private:
+    explicit SGIVideoSyncVsyncMonitor(QObject *parent = nullptr);
+
+    QThread *m_thread = nullptr;
+    SGIVideoSyncVsyncMonitorHelper *m_helper = nullptr;
+};
+
+} // namespace KWin

arkroyal01/volcanic

plugins/platforms/drm/drm_backend.cpp
@@ -16,6 +16,7 @@
 #include "logging.h"
 #include "logind.h"
 #include "main.h"
+#include "renderloop_p.h"
 #include "scene_qpainter_drm_backend.h"
 #include "udev.h"
 #include "wayland_server.h"
@@ -188,8 +189,11 @@ void DrmBackend::reactivate()
     }
     // restart compositor
     m_pageFlipsPending = 0;
+
+    for (DrmOutput *output : qAsConst(m_outputs)) {
+        output->renderLoop()->uninhibit();
+    }
     if (Compositor *compositor = Compositor::self()) {
-        compositor->bufferSwapComplete();
         compositor->addRepaintFull();
     }
 }
@@ -199,36 +203,61 @@ void DrmBackend::deactivate()
     if (!m_active) {
         return;
     }
-    // block compositor
-    if (m_pageFlipsPending == 0 && Compositor::self()) {
-        Compositor::self()->aboutToSwapBuffers();
+
+    for (DrmOutput *output : qAsConst(m_outputs)) {
+        output->hideCursor();
+        output->renderLoop()->inhibit();
     }
-    // hide cursor and disable
-    for (auto it = m_outputs.constBegin(); it != m_outputs.constEnd(); ++it) {
-        DrmOutput *o = *it;
-        o->hideCursor();
-    }
+
     m_active = false;
+}
+
+static std::chrono::nanoseconds convertTimestamp(const timespec &timestamp)
+{
+    return std::chrono::seconds(timestamp.tv_sec) + std::chrono::nanoseconds(timestamp.tv_nsec);
+}
+
+static std::chrono::nanoseconds convertTimestamp(clockid_t sourceClock, clockid_t targetClock,
+                                                 const timespec &timestamp)
+{
+    if (sourceClock == targetClock) {
+        return convertTimestamp(timestamp);
+    }
+
+    timespec sourceCurrentTime = {};
+    timespec targetCurrentTime = {};
+
+    clock_gettime(sourceClock, &sourceCurrentTime);
+    clock_gettime(targetClock, &targetCurrentTime);
+
+    const auto delta = convertTimestamp(sourceCurrentTime) - convertTimestamp(timestamp);
+    return convertTimestamp(targetCurrentTime) - delta;
 }
 
 void DrmBackend::pageFlipHandler(int fd, unsigned int frame, unsigned int sec, unsigned int usec, void *data)
 {
     Q_UNUSED(fd)
     Q_UNUSED(frame)
-    Q_UNUSED(sec)
-    Q_UNUSED(usec)
-    auto output = reinterpret_cast<DrmOutput*>(data);
+
+    auto output = static_cast<DrmOutput *>(data);
+
+    DrmGpu *gpu = output->gpu();
+    DrmBackend *backend = output->m_backend;
+
+    std::chrono::nanoseconds timestamp = convertTimestamp(gpu->presentationClock(),
+                                                          CLOCK_MONOTONIC,
+                                                          { sec, usec * 1000 });
+    if (timestamp == std::chrono::nanoseconds::zero()) {
+        qCDebug(KWIN_DRM, "Got invalid timestamp (sec: %u, usec: %u) on output %s",
+                sec, usec, qPrintable(output->name()));
+        timestamp = std::chrono::steady_clock::now().time_since_epoch();
+    }
 
     output->pageFlipped();
-    output->m_backend->m_pageFlipsPending--;
-    if (output->m_backend->m_pageFlipsPending == 0) {
-        // TODO: improve, this currently means we wait for all page flips or all outputs.
-        // It would be better to driver the repaint per output
+    backend->m_pageFlipsPending--;
 
-        if (Compositor::self()) {
-            Compositor::self()->bufferSwapComplete();
-        }
-    }
+    RenderLoopPrivate *renderLoopPrivate = RenderLoopPrivate::get(output->renderLoop());
+    renderLoopPrivate->notifyFrameCompleted(timestamp);
 }
 
 void DrmBackend::openDrm()
@@ -526,9 +555,6 @@ bool DrmBackend::present(DrmBuffer *buffer, DrmOutput *output)
 
     if (output->present(buffer)) {
         m_pageFlipsPending++;
-        if (m_pageFlipsPending == 1 && Compositor::self()) {
-            Compositor::self()->aboutToSwapBuffers();
-        }
         return true;
     } else if (output->gpu()->deleteBufferAfterPageFlip()) {
         delete buffer;

plugins/platforms/virtual/scene_qpainter_virtual_backend.cpp
@@ -7,9 +7,11 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "scene_qpainter_virtual_backend.h"
-#include "virtual_backend.h"
 #include "cursor.h"
 #include "screens.h"
+#include "softwarevsyncmonitor.h"
+#include "virtual_backend.h"
+#include "virtual_output.h"
 
 #include <QPainter>
 
@@ -55,6 +57,10 @@ void VirtualQPainterBackend::endFrame(int screenId, int mask, const QRegion &dam
 {
     Q_UNUSED(mask)
     Q_UNUSED(damage)
+
+    VirtualOutput *output = static_cast<VirtualOutput *>(m_backend->findOutput(screenId));
+    output->vsyncMonitor()->arm();
+
     if (m_backend->saveFrames()) {
         m_backBuffers[screenId].save(QStringLiteral("%1/screen%2-%3.png").arg(m_backend->screenshotDirPath(), QString::number(screenId), QString::number(m_frameCounter++)));
     }

plugins/platforms/x11/standalone/sgivideosyncvsyncmonitor.h
@@ -1,0 +1,75 @@
+/*
+    SPDX-FileCopyrightText: 2020 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "vsyncmonitor.h"
+
+#include <epoxy/glx.h>
+#include <GL/glxext.h>
+#include "fixx11h.h"
+
+#include <QThread>
+
+namespace KWin
+{
+
+/**
+ * The SGIVideoSyncVsyncMonitorHelper class is responsible for waiting for vsync events on the
+ * root window. Note that the helper runs on a separate thread.
+ */
+class SGIVideoSyncVsyncMonitorHelper : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit SGIVideoSyncVsyncMonitorHelper(QObject *parent = nullptr);
+    ~SGIVideoSyncVsyncMonitorHelper() override;
+
+    bool isValid() const;
+
+public Q_SLOTS:
+    void poll();
+
+Q_SIGNALS:
+    void errorOccurred();
+    void vblankOccurred(std::chrono::nanoseconds timestamp);
+
+private:
+    Display *m_display = nullptr;
+    GLXContext m_localContext = 0;
+    GLXDrawable m_drawable = 0;
+};
+
+/**
+ * The SGIVideoSyncVsyncMonitor class monitors vblank events using the GLX_SGI_video_sync
+ * extension.
+ *
+ * Vblank events are monitored in a separated thread to avoid blocking the main thread. In
+ * order to avoid locking up the main X11 connection, the worker thread establishes its own
+ * X11 connection.
+ */
+class SGIVideoSyncVsyncMonitor : public VsyncMonitor
+{
+    Q_OBJECT
+
+public:
+    static SGIVideoSyncVsyncMonitor *create(QObject *parent);
+    ~SGIVideoSyncVsyncMonitor() override;
+
+    bool isValid() const;
+
+public Q_SLOTS:
+    void arm() override;
+
+private:
+    explicit SGIVideoSyncVsyncMonitor(QObject *parent = nullptr);
+
+    QThread *m_thread = nullptr;
+    SGIVideoSyncVsyncMonitorHelper *m_helper = nullptr;
+};
+
+} // namespace KWin