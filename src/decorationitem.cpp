dingjingmaster/kwin

scene: Introduce window items
Currently, dealing with sub-surfaces is very difficult due to the scene
design being heavily influenced by X11 requirements.

The goal of this change is to re-work scene abstractions to make improving
the wayland support easier.

The Item class is based on the QQuickItem class. My hope is that one day
we will be able to transition to QtQuick for painting scene, but in
meanwhile it makes more sense to have a minimalistic internal item class.

The WindowItem class represents a window. The SurfaceItem class represents
the contents of either an X11, or a Wayland, or an internal surface. The
DecorationItem and the ShadowItem class represent the server-side deco and
drop-shadow, respectively.

At the moment, the SurfaceItem is bound to the scene window, but the long
term plan is to break that connection so we could re-use the SurfaceItem
for things such as software cursors and drag-and-drop additional icons.

One of the responsibilities of the Item is to schedule repaints as needed.
Ideally, there shouldn't be any addRepaint() calls in the core code. The
Item class schedules repaints on geometry updates. In the future, it also
has to request an update if its opacity or visibility changes.

decorationitem.cpp
@@ -1,0 +1,35 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "decorationitem.h"
+
+#include <KDecoration2/Decoration>
+
+namespace KWin
+{
+
+DecorationItem::DecorationItem(KDecoration2::Decoration *decoration, Scene::Window *window, Item *parent)
+    : Item(window, parent)
+{
+    Toplevel *toplevel = window->window();
+
+    connect(toplevel, &Toplevel::frameGeometryChanged,
+            this, &DecorationItem::handleFrameGeometryChanged);
+
+    connect(toplevel, &Toplevel::screenScaleChanged,
+            this, &DecorationItem::discardQuads);
+    connect(decoration, &KDecoration2::Decoration::bordersChanged,
+            this, &DecorationItem::discardQuads);
+
+    setImplicitSize(toplevel->size());
+}
+
+void DecorationItem::handleFrameGeometryChanged()
+{
+    setImplicitSize(window()->size());
+}
+
+} // namespace KWin

surfaceitem_wayland.cpp
@@ -1,0 +1,150 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "surfaceitem_wayland.h"
+
+#include <KWaylandServer/subcompositor_interface.h>
+#include <KWaylandServer/surface_interface.h>
+
+namespace KWin
+{
+
+SurfaceItemWayland::SurfaceItemWayland(KWaylandServer::SurfaceInterface *surface,
+                                       Scene::Window *window, Item *parent)
+    : SurfaceItem(window, parent)
+    , m_surface(surface)
+{
+    connect(surface, &KWaylandServer::SurfaceInterface::surfaceToBufferMatrixChanged,
+            this, &SurfaceItemWayland::discardQuads);
+    connect(surface, &KWaylandServer::SurfaceInterface::surfaceToBufferMatrixChanged,
+            this, &SurfaceItemWayland::discardPixmap);
+
+    connect(surface, &KWaylandServer::SurfaceInterface::sizeChanged,
+            this, &SurfaceItemWayland::handleSurfaceSizeChanged);
+    connect(surface, &KWaylandServer::SurfaceInterface::bufferSizeChanged,
+            this, &SurfaceItemWayland::discardPixmap);
+
+    connect(surface, &KWaylandServer::SurfaceInterface::childSubSurfacesChanged,
+            this, &SurfaceItemWayland::handleChildSubSurfacesChanged);
+    connect(surface, &KWaylandServer::SurfaceInterface::committed,
+            this, &SurfaceItemWayland::handleSurfaceCommitted);
+    connect(surface, &KWaylandServer::SurfaceInterface::damaged,
+            this, &SurfaceItemWayland::addDamage);
+    connect(surface, &KWaylandServer::SurfaceInterface::childSubSurfaceAdded,
+            this, &SurfaceItemWayland::handleChildSubSurfaceAdded);
+    connect(surface, &KWaylandServer::SurfaceInterface::childSubSurfaceRemoved,
+            this, &SurfaceItemWayland::handleChildSubSurfaceRemoved);
+
+    KWaylandServer::SubSurfaceInterface *subsurface = surface->subSurface();
+    if (subsurface) {
+        connect(subsurface, &KWaylandServer::SubSurfaceInterface::positionChanged,
+                this, &SurfaceItemWayland::handleSubSurfacePositionChanged);
+        setImplicitPosition(subsurface->position());
+    }
+
+    const QList<KWaylandServer::SubSurfaceInterface *> children = surface->childSubSurfaces();
+    for (KWaylandServer::SubSurfaceInterface *subsurface : children) {
+        handleChildSubSurfaceAdded(subsurface);
+    }
+
+    setImplicitSize(surface->size());
+}
+
+QPointF SurfaceItemWayland::mapToBuffer(const QPointF &point) const
+{
+    if (m_surface) {
+        return m_surface->mapToBuffer(point);
+    }
+    return point;
+}
+
+QRegion SurfaceItemWayland::shape() const
+{
+    return QRegion(0, 0, width(), height());
+}
+
+QRegion SurfaceItemWayland::opaque() const
+{
+    if (m_surface) {
+        return m_surface->opaque();
+    }
+    return QRegion();
+}
+
+KWaylandServer::SurfaceInterface *SurfaceItemWayland::surface() const
+{
+    return m_surface;
+}
+
+void SurfaceItemWayland::handleSurfaceSizeChanged()
+{
+    setImplicitSize(m_surface->size());
+}
+
+void SurfaceItemWayland::handleSurfaceCommitted()
+{
+    if (m_surface->hasFrameCallbacks()) {
+        scheduleRepaint();
+    }
+}
+
+void SurfaceItemWayland::handleChildSubSurfaceAdded(KWaylandServer::SubSurfaceInterface *child)
+{
+    SurfaceItemWayland *subsurfaceItem = new SurfaceItemWayland(child->surface(), window());
+    subsurfaceItem->setParent(this);
+    subsurfaceItem->setParentItem(this);
+
+    m_subsurfaces.insert(child, subsurfaceItem);
+}
+
+void SurfaceItemWayland::handleChildSubSurfaceRemoved(KWaylandServer::SubSurfaceInterface *child)
+{
+    delete m_subsurfaces.take(child);
+}
+
+void SurfaceItemWayland::handleChildSubSurfacesChanged()
+{
+    const QList<KWaylandServer::SubSurfaceInterface *> stackingOrder = m_surface->childSubSurfaces();
+    m_childItems.clear();
+
+    for (KWaylandServer::SubSurfaceInterface *subsurface : stackingOrder) {
+        m_childItems.append(m_subsurfaces[subsurface]);
+    }
+
+    discardQuads();
+}
+
+void SurfaceItemWayland::handleSubSurfacePositionChanged()
+{
+    setImplicitPosition(m_surface->subSurface()->position());
+}
+
+WindowPixmap *SurfaceItemWayland::createPixmap()
+{
+    WindowPixmap *pixmap = window()->createWindowPixmap();
+    pixmap->setSurface(m_surface);
+    return pixmap;
+}
+
+SurfaceItemXwayland::SurfaceItemXwayland(Scene::Window *window, Item *parent)
+    : SurfaceItemWayland(window->window()->surface(), window, parent)
+{
+}
+
+QRegion SurfaceItemXwayland::shape() const
+{
+    const Toplevel *toplevel = window()->window();
+    if (window()->isShaded()) {
+        return QRegion();
+    }
+
+    const QRect clipRect = toplevel->clientGeometry().translated(-toplevel->bufferGeometry().topLeft());
+    const QRegion shape = toplevel->shapeRegion();
+
+    return shape & clipRect;
+}
+
+} // namespace KWin

surfaceitem_x11.cpp
@@ -1,0 +1,142 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "surfaceitem_x11.h"
+
+namespace KWin
+{
+
+SurfaceItemX11::SurfaceItemX11(Scene::Window *window, Item *parent)
+    : SurfaceItem(window, parent)
+{
+    Toplevel *toplevel = window->window();
+
+    connect(toplevel, &Toplevel::bufferGeometryChanged,
+            this, &SurfaceItemX11::handleBufferGeometryChanged);
+    connect(toplevel, &Toplevel::geometryShapeChanged,
+            this, &SurfaceItemX11::discardQuads);
+
+    m_damageHandle = xcb_generate_id(kwinApp()->x11Connection());
+    xcb_damage_create(kwinApp()->x11Connection(), m_damageHandle, toplevel->frameId(),
+                      XCB_DAMAGE_REPORT_LEVEL_NON_EMPTY);
+
+    setImplicitSize(toplevel->bufferGeometry().size());
+}
+
+SurfaceItemX11::~SurfaceItemX11()
+{
+    Q_ASSERT_X(m_damageHandle == XCB_NONE, Q_FUNC_INFO, "damage is not destroyed");
+}
+
+void SurfaceItemX11::processDamage()
+{
+    m_isDamaged = true;
+    scheduleRepaint();
+}
+
+bool SurfaceItemX11::fetchDamage()
+{
+    if (!m_isDamaged) {
+        return false;
+    }
+    m_isDamaged = false;
+
+    if (m_damageHandle == XCB_NONE) {
+        return true;
+    }
+
+    xcb_xfixes_region_t region = xcb_generate_id(kwinApp()->x11Connection());
+    xcb_xfixes_create_region(kwinApp()->x11Connection(), region, 0, nullptr);
+    xcb_damage_subtract(kwinApp()->x11Connection(), m_damageHandle, 0, region);
+
+    m_damageCookie = xcb_xfixes_fetch_region_unchecked(kwinApp()->x11Connection(), region);
+    xcb_xfixes_destroy_region(kwinApp()->x11Connection(), region);
+
+    m_havePendingDamageRegion = true;
+
+    return true;
+}
+
+void SurfaceItemX11::waitForDamage()
+{
+    if (!m_havePendingDamageRegion) {
+        return;
+    }
+    m_havePendingDamageRegion = false;
+
+    xcb_xfixes_fetch_region_reply_t *reply =
+            xcb_xfixes_fetch_region_reply(kwinApp()->x11Connection(), m_damageCookie, nullptr);
+    if (!reply) {
+        qCDebug(KWIN_CORE) << "Failed to check damage region";
+        return;
+    }
+
+    const int rectCount = xcb_xfixes_fetch_region_rectangles_length(reply);
+    QRegion region;
+
+    if (rectCount > 1 && rectCount < 16) {
+        xcb_rectangle_t *rects = xcb_xfixes_fetch_region_rectangles(reply);
+
+        QVector<QRect> qtRects;
+        qtRects.reserve(rectCount);
+
+        for (int i = 0; i < rectCount; ++i) {
+            qtRects << QRect(rects[i].x, rects[i].y, rects[i].width, rects[i].height);
+        }
+        region.setRects(qtRects.constData(), rectCount);
+    } else {
+        region = QRect(reply->extents.x, reply->extents.y, reply->extents.width, reply->extents.height);
+    }
+    free(reply);
+
+    addDamage(region);
+}
+
+void SurfaceItemX11::destroyDamage()
+{
+    if (m_damageHandle != XCB_NONE) {
+        xcb_damage_destroy(kwinApp()->x11Connection(), m_damageHandle);
+        m_damageHandle = XCB_NONE;
+    }
+}
+
+void SurfaceItemX11::handleBufferGeometryChanged(Toplevel *toplevel, const QRect &old)
+{
+    if (toplevel->bufferGeometry().size() != old.size()) {
+        discardPixmap();
+    }
+    setImplicitSize(toplevel->bufferGeometry().size());
+}
+
+QPointF SurfaceItemX11::mapToBuffer(const QPointF &point) const
+{
+    return point;
+}
+
+QRegion SurfaceItemX11::shape() const
+{
+    const Toplevel *toplevel = window()->window();
+    if (window()->isShaded()) {
+        return QRegion();
+    }
+
+    const QRect clipRect = toplevel->clientGeometry().translated(-toplevel->bufferGeometry().topLeft());
+    const QRegion shape = toplevel->shapeRegion();
+
+    return shape & clipRect;
+}
+
+QRegion SurfaceItemX11::opaque() const
+{
+    return window()->window()->opaqueRegion();
+}
+
+WindowPixmap *SurfaceItemX11::createPixmap()
+{
+    return window()->createWindowPixmap();
+}
+
+} // namespace KWin

arkroyal01/volcanic

src/decorationitem.cpp
@@ -1,0 +1,35 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "decorationitem.h"
+
+#include <KDecoration2/Decoration>
+
+namespace KWin
+{
+
+DecorationItem::DecorationItem(KDecoration2::Decoration *decoration, Scene::Window *window, Item *parent)
+    : Item(window, parent)
+{
+    Toplevel *toplevel = window->window();
+
+    connect(toplevel, &Toplevel::frameGeometryChanged,
+            this, &DecorationItem::handleFrameGeometryChanged);
+
+    connect(toplevel, &Toplevel::screenScaleChanged,
+            this, &DecorationItem::discardQuads);
+    connect(decoration, &KDecoration2::Decoration::bordersChanged,
+            this, &DecorationItem::discardQuads);
+
+    setSize(toplevel->size());
+}
+
+void DecorationItem::handleFrameGeometryChanged()
+{
+    setSize(window()->size());
+}
+
+} // namespace KWin

src/surfaceitem_wayland.cpp
@@ -1,0 +1,151 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "surfaceitem_wayland.h"
+
+#include <KWaylandServer/subcompositor_interface.h>
+#include <KWaylandServer/surface_interface.h>
+
+namespace KWin
+{
+
+SurfaceItemWayland::SurfaceItemWayland(KWaylandServer::SurfaceInterface *surface,
+                                       Scene::Window *window, Item *parent)
+    : SurfaceItem(window, parent)
+    , m_surface(surface)
+{
+    connect(surface, &KWaylandServer::SurfaceInterface::surfaceToBufferMatrixChanged,
+            this, &SurfaceItemWayland::discardQuads);
+    connect(surface, &KWaylandServer::SurfaceInterface::surfaceToBufferMatrixChanged,
+            this, &SurfaceItemWayland::discardPixmap);
+
+    connect(surface, &KWaylandServer::SurfaceInterface::sizeChanged,
+            this, &SurfaceItemWayland::handleSurfaceSizeChanged);
+    connect(surface, &KWaylandServer::SurfaceInterface::bufferSizeChanged,
+            this, &SurfaceItemWayland::discardPixmap);
+
+    connect(surface, &KWaylandServer::SurfaceInterface::childSubSurfacesChanged,
+            this, &SurfaceItemWayland::handleChildSubSurfacesChanged);
+    connect(surface, &KWaylandServer::SurfaceInterface::committed,
+            this, &SurfaceItemWayland::handleSurfaceCommitted);
+    connect(surface, &KWaylandServer::SurfaceInterface::damaged,
+            this, &SurfaceItemWayland::addDamage);
+    connect(surface, &KWaylandServer::SurfaceInterface::childSubSurfaceAdded,
+            this, &SurfaceItemWayland::handleChildSubSurfaceAdded);
+    connect(surface, &KWaylandServer::SurfaceInterface::childSubSurfaceRemoved,
+            this, &SurfaceItemWayland::handleChildSubSurfaceRemoved);
+
+    KWaylandServer::SubSurfaceInterface *subsurface = surface->subSurface();
+    if (subsurface) {
+        connect(subsurface, &KWaylandServer::SubSurfaceInterface::positionChanged,
+                this, &SurfaceItemWayland::handleSubSurfacePositionChanged);
+        setPosition(subsurface->position());
+    }
+
+    const QList<KWaylandServer::SubSurfaceInterface *> children = surface->childSubSurfaces();
+    for (KWaylandServer::SubSurfaceInterface *subsurface : children) {
+        handleChildSubSurfaceAdded(subsurface);
+    }
+
+    setSize(surface->size());
+}
+
+QPointF SurfaceItemWayland::mapToBuffer(const QPointF &point) const
+{
+    if (m_surface) {
+        return m_surface->mapToBuffer(point);
+    }
+    return point;
+}
+
+QRegion SurfaceItemWayland::shape() const
+{
+    return QRegion(0, 0, width(), height());
+}
+
+QRegion SurfaceItemWayland::opaque() const
+{
+    if (m_surface) {
+        return m_surface->opaque();
+    }
+    return QRegion();
+}
+
+KWaylandServer::SurfaceInterface *SurfaceItemWayland::surface() const
+{
+    return m_surface;
+}
+
+void SurfaceItemWayland::handleSurfaceSizeChanged()
+{
+    setSize(m_surface->size());
+}
+
+void SurfaceItemWayland::handleSurfaceCommitted()
+{
+    if (m_surface->hasFrameCallbacks()) {
+        scheduleRepaint();
+    }
+}
+
+void SurfaceItemWayland::handleChildSubSurfaceAdded(KWaylandServer::SubSurfaceInterface *child)
+{
+    SurfaceItemWayland *subsurfaceItem = new SurfaceItemWayland(child->surface(), window());
+    subsurfaceItem->setParent(this);
+    subsurfaceItem->setParentItem(this);
+
+    m_subsurfaces.insert(child, subsurfaceItem);
+}
+
+void SurfaceItemWayland::handleChildSubSurfaceRemoved(KWaylandServer::SubSurfaceInterface *child)
+{
+    delete m_subsurfaces.take(child);
+}
+
+void SurfaceItemWayland::handleChildSubSurfacesChanged()
+{
+    const QList<KWaylandServer::SubSurfaceInterface *> stackingOrder = m_surface->childSubSurfaces();
+    QList<Item *> items;
+    items.reserve(stackingOrder.count());
+
+    for (KWaylandServer::SubSurfaceInterface *subsurface : stackingOrder) {
+        items.append(m_subsurfaces[subsurface]);
+    }
+
+    stackChildren(items);
+}
+
+void SurfaceItemWayland::handleSubSurfacePositionChanged()
+{
+    setPosition(m_surface->subSurface()->position());
+}
+
+WindowPixmap *SurfaceItemWayland::createPixmap()
+{
+    WindowPixmap *pixmap = window()->createWindowPixmap();
+    pixmap->setSurface(m_surface);
+    return pixmap;
+}
+
+SurfaceItemXwayland::SurfaceItemXwayland(Scene::Window *window, Item *parent)
+    : SurfaceItemWayland(window->window()->surface(), window, parent)
+{
+}
+
+QRegion SurfaceItemXwayland::shape() const
+{
+    const Toplevel *toplevel = window()->window();
+    if (window()->isShaded()) {
+        return QRegion();
+    }
+
+    const QRect clipRect = toplevel->clientGeometry().translated(-toplevel->bufferGeometry().topLeft());
+    const QRegion shape = toplevel->shapeRegion();
+
+    return shape & clipRect;
+}
+
+} // namespace KWin

src/surfaceitem_x11.cpp
@@ -1,0 +1,141 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "surfaceitem_x11.h"
+
+namespace KWin
+{
+
+SurfaceItemX11::SurfaceItemX11(Scene::Window *window, Item *parent)
+    : SurfaceItem(window, parent)
+{
+    Toplevel *toplevel = window->window();
+
+    connect(toplevel, &Toplevel::bufferGeometryChanged,
+            this, &SurfaceItemX11::handleBufferGeometryChanged);
+    connect(toplevel, &Toplevel::geometryShapeChanged,
+            this, &SurfaceItemX11::discardQuads);
+
+    m_damageHandle = xcb_generate_id(kwinApp()->x11Connection());
+    xcb_damage_create(kwinApp()->x11Connection(), m_damageHandle, toplevel->frameId(),
+                      XCB_DAMAGE_REPORT_LEVEL_NON_EMPTY);
+
+    setSize(toplevel->bufferGeometry().size());
+}
+
+SurfaceItemX11::~SurfaceItemX11()
+{
+}
+
+void SurfaceItemX11::processDamage()
+{
+    m_isDamaged = true;
+    scheduleRepaint();
+}
+
+bool SurfaceItemX11::fetchDamage()
+{
+    if (!m_isDamaged) {
+        return false;
+    }
+    m_isDamaged = false;
+
+    if (m_damageHandle == XCB_NONE) {
+        return true;
+    }
+
+    xcb_xfixes_region_t region = xcb_generate_id(kwinApp()->x11Connection());
+    xcb_xfixes_create_region(kwinApp()->x11Connection(), region, 0, nullptr);
+    xcb_damage_subtract(kwinApp()->x11Connection(), m_damageHandle, 0, region);
+
+    m_damageCookie = xcb_xfixes_fetch_region_unchecked(kwinApp()->x11Connection(), region);
+    xcb_xfixes_destroy_region(kwinApp()->x11Connection(), region);
+
+    m_havePendingDamageRegion = true;
+
+    return true;
+}
+
+void SurfaceItemX11::waitForDamage()
+{
+    if (!m_havePendingDamageRegion) {
+        return;
+    }
+    m_havePendingDamageRegion = false;
+
+    xcb_xfixes_fetch_region_reply_t *reply =
+            xcb_xfixes_fetch_region_reply(kwinApp()->x11Connection(), m_damageCookie, nullptr);
+    if (!reply) {
+        qCDebug(KWIN_CORE) << "Failed to check damage region";
+        return;
+    }
+
+    const int rectCount = xcb_xfixes_fetch_region_rectangles_length(reply);
+    QRegion region;
+
+    if (rectCount > 1 && rectCount < 16) {
+        xcb_rectangle_t *rects = xcb_xfixes_fetch_region_rectangles(reply);
+
+        QVector<QRect> qtRects;
+        qtRects.reserve(rectCount);
+
+        for (int i = 0; i < rectCount; ++i) {
+            qtRects << QRect(rects[i].x, rects[i].y, rects[i].width, rects[i].height);
+        }
+        region.setRects(qtRects.constData(), rectCount);
+    } else {
+        region = QRect(reply->extents.x, reply->extents.y, reply->extents.width, reply->extents.height);
+    }
+    free(reply);
+
+    addDamage(region);
+}
+
+void SurfaceItemX11::destroyDamage()
+{
+    if (m_damageHandle != XCB_NONE) {
+        xcb_damage_destroy(kwinApp()->x11Connection(), m_damageHandle);
+        m_damageHandle = XCB_NONE;
+    }
+}
+
+void SurfaceItemX11::handleBufferGeometryChanged(Toplevel *toplevel, const QRect &old)
+{
+    if (toplevel->bufferGeometry().size() != old.size()) {
+        discardPixmap();
+    }
+    setSize(toplevel->bufferGeometry().size());
+}
+
+QPointF SurfaceItemX11::mapToBuffer(const QPointF &point) const
+{
+    return point;
+}
+
+QRegion SurfaceItemX11::shape() const
+{
+    const Toplevel *toplevel = window()->window();
+    if (window()->isShaded()) {
+        return QRegion();
+    }
+
+    const QRect clipRect = toplevel->clientGeometry().translated(-toplevel->bufferGeometry().topLeft());
+    const QRegion shape = toplevel->shapeRegion();
+
+    return shape & clipRect;
+}
+
+QRegion SurfaceItemX11::opaque() const
+{
+    return window()->window()->opaqueRegion();
+}
+
+WindowPixmap *SurfaceItemX11::createPixmap()
+{
+    return window()->createWindowPixmap();
+}
+
+} // namespace KWin