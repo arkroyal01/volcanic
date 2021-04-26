paulmcauley/kwin

Restore following XDG_SESSION_ID env if set
BUG: 435468

Testing done:
Ran on a VT with an XDG_SESSION_ID from another terminal, ensured it
switched

src/session_logind.cpp
@@ -61,11 +61,19 @@ static const QString s_managerPath = QStringLiteral("/org/freedesktop/login1");
 
 static QString findProcessSessionPath()
 {
-    QDBusMessage message = QDBusMessage::createMethodCall(s_serviceName, s_managerPath,
-                                                          s_managerInterface,
-                                                          QStringLiteral("GetSessionByPID"));
-    message.setArguments({ uint32_t(QCoreApplication::applicationPid()) });
-
+    const QByteArray sessionId = qgetenv("XDG_SESSION_ID");
+    QDBusMessage message;
+    if (sessionId.isEmpty()) {
+        message = QDBusMessage::createMethodCall(s_serviceName, s_managerPath,
+                                                            s_managerInterface,
+                                                            QStringLiteral("GetSessionByPID"));
+        message.setArguments({ uint32_t(QCoreApplication::applicationPid()) });
+    } else {
+        message = QDBusMessage::createMethodCall(s_serviceName, s_managerPath,
+                                                            s_managerInterface,
+                                                            QStringLiteral("GetSession"));
+        message.setArguments({QString::fromLocal8Bit(sessionId)});
+    }
     const QDBusMessage reply = QDBusConnection::systemBus().call(message);
     if (reply.type() == QDBusMessage::ErrorMessage) {
         return QString();

arkroyal01/volcanic

src/session_logind.cpp
@@ -61,11 +61,19 @@ static const QString s_managerPath = QStringLiteral("/org/freedesktop/login1");
 
 static QString findProcessSessionPath()
 {
-    QDBusMessage message = QDBusMessage::createMethodCall(s_serviceName, s_managerPath,
-                                                          s_managerInterface,
-                                                          QStringLiteral("GetSessionByPID"));
-    message.setArguments({ uint32_t(QCoreApplication::applicationPid()) });
-
+    const QString sessionId = qEnvironmentVariable("XDG_SESSION_ID");
+    QDBusMessage message;
+    if (sessionId.isEmpty()) {
+        message = QDBusMessage::createMethodCall(s_serviceName, s_managerPath,
+                                                 s_managerInterface,
+                                                 QStringLiteral("GetSessionByPID"));
+        message.setArguments({uint32_t(QCoreApplication::applicationPid())});
+    } else {
+        message = QDBusMessage::createMethodCall(s_serviceName, s_managerPath,
+                                                 s_managerInterface,
+                                                 QStringLiteral("GetSession"));
+        message.setArguments({sessionId});
+    }
     const QDBusMessage reply = QDBusConnection::systemBus().call(message);
     if (reply.type() == QDBusMessage::ErrorMessage) {
         return QString();