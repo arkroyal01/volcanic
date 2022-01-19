KDE/kwin

commit 19c471405e7eb4b6026db24d776d205125dbc013

backends/drm: if multi-gpu copy fails, disable deep color
The format XRGB8888 is effectively universally supported. If everything
else fails, try to fall back to that


(cherry picked from commit 0ecfbb0d1cc9af9c22a8783212e735763bf05555)

src/backends/drm/egl_gbm_backend.cpp
@@ -334,8 +334,9 @@ QSharedPointer<DrmBuffer> EglGbmBackend::importFramebuffer(Output &output, const
     }
     // ImportMode::DumbBuffer
     if (!output.current.importSwapchain || output.current.importSwapchain->size() != size) {
-        output.current.importSwapchain = QSharedPointer<DumbSwapchain>::create(m_gpu, size, renderingBackend()->drmFormat(output.output));
-        if (output.current.importSwapchain->isEmpty()) {
+        const uint32_t format = renderingBackend()->drmFormat(output.output);
+        output.current.importSwapchain = QSharedPointer<DumbSwapchain>::create(m_gpu, size, format);
+        if (output.current.importSwapchain->isEmpty() || output.current.importSwapchain->currentBuffer()->format() != format) {
             output.current.importSwapchain = nullptr;
         }
     }
@@ -346,7 +347,8 @@ QSharedPointer<DrmBuffer> EglGbmBackend::importFramebuffer(Output &output, const
         }
     }
     qCWarning(KWIN_DRM) << "all imports failed on output" << output.output;
-    // TODO turn off output?
+    // try again with XRGB8888, the most universally supported basic format
+    output.forceXrgb8888 = true;
     return nullptr;
 }
 
@@ -542,18 +544,21 @@ QRegion EglGbmBackend::beginFrame(AbstractOutput *drmOutput)
     }
 }
 
-bool EglGbmBackend::doesRenderFit(DrmAbstractOutput *output, const Output::RenderData &render)
+bool EglGbmBackend::doesRenderFit(const Output &output, const Output::RenderData &render)
 {
     if (!render.gbmSurface) {
         return false;
     }
-    QSize surfaceSize = output->bufferSize();
+    if (output.forceXrgb8888 && render.gbmSurface->format() != DRM_FORMAT_XRGB8888) {
+        return false;
+    }
+    QSize surfaceSize = output.output->bufferSize();
     if (surfaceSize != render.gbmSurface->size()) {
         return false;
     }
-    bool needsTexture = output->needsSoftwareTransformation();
+    bool needsTexture = output.output->needsSoftwareTransformation();
     if (needsTexture) {
-        return render.shadowBuffer && render.shadowBuffer->textureSize() == output->sourceSize();
+        return render.shadowBuffer && render.shadowBuffer->textureSize() == output.output->sourceSize();
     } else {
         return render.shadowBuffer == nullptr;
     }
@@ -562,8 +567,8 @@ bool EglGbmBackend::doesRenderFit(DrmAbstractOutput *output, const Output::Rende
 QRegion EglGbmBackend::prepareRenderingForOutput(Output &output)
 {
     // check if the current surface still fits
-    if (!doesRenderFit(output.output, output.current)) {
-        if (doesRenderFit(output.output, output.old)) {
+    if (!doesRenderFit(output, output.current)) {
+        if (doesRenderFit(output, output.old)) {
             cleanupRenderData(output.current);
             output.current = output.old;
             output.old = {};
@@ -763,7 +768,15 @@ QSharedPointer<DrmBuffer> EglGbmBackend::renderTestFrame(DrmAbstractOutput *outp
 {
     beginFrame(output);
     glClear(GL_COLOR_BUFFER_BIT);
-    return endFrameWithBuffer(output, output->geometry());
+    const auto buffer = endFrameWithBuffer(output, output->geometry());
+    if (!buffer && this != renderingBackend()) {
+        // if CPU import fails we might need to try again
+        beginFrame(output);
+        glClear(GL_COLOR_BUFFER_BIT);
+        return endFrameWithBuffer(output, output->geometry());
+    } else {
+        return buffer;
+    }
 }
 
 QSharedPointer<GLTexture> EglGbmBackend::textureForOutput(AbstractOutput *output) const
@@ -812,6 +825,15 @@ DrmGpu *EglGbmBackend::gpu() const
 
 std::optional<GbmFormat> EglGbmBackend::chooseFormat(Output &output) const
 {
+    if (output.forceXrgb8888) {
+        return GbmFormat {
+            .drmFormat = DRM_FORMAT_XRGB8888,
+            .redSize = 8,
+            .greenSize = 8,
+            .blueSize = 8,
+            .alphaSize = 0,
+        };
+    }
     // formats are already sorted by order of preference
     std::optional<GbmFormat> fallback;
     for (const auto &format : qAsConst(m_formats)) {

src/backends/drm/egl_gbm_backend.h
@@ -95,6 +95,7 @@ private:
     };
     struct Output {
         DrmAbstractOutput *output = nullptr;
+        bool forceXrgb8888 = false;
         struct RenderData {
             QSharedPointer<ShadowBuffer> shadowBuffer;
             QSharedPointer<GbmSurface> gbmSurface;
@@ -115,7 +116,7 @@ private:
         QPointer<KWaylandServer::SurfaceInterface> oldScanoutCandidate;
     };
 
-    bool doesRenderFit(DrmAbstractOutput *output, const Output::RenderData &render);
+    bool doesRenderFit(const Output &output, const Output::RenderData &render);
     bool resetOutput(Output &output);
     bool addOutput(DrmAbstractOutput *output);
     void removeOutput(DrmAbstractOutput *output);

arkroyal01/volcanic

commit a54c4ef65391a43c7daffc0731e086fbd0c57b7d

Fix force xrgb 8888
19c471405e7eb4b6026db24d776d205125dbc013 introduced a regression if
there are two gbm backend and the backend fail to choose drm format.

This fix does two things:
1. Current buffer format should not be reset after create new buffer,
   otherwise current.format may just be empty after resetOutput.
2. force xrgb 8888 need to be set on the primary backend.

BUG: 448790

src/backends/drm/egl_gbm_backend.cpp
@@ -155,7 +155,6 @@ bool EglGbmBackend::resetOutput(Output &output)
         qCCritical(KWIN_DRM) << "Could not find a suitable format for output" << output.output;
         return false;
     }
-    output.current.format = gbmFormat.value();
     uint32_t format = gbmFormat.value().drmFormat;
     QVector<uint64_t> modifiers = output.output->supportedModifiers(format);
     const QSize size = output.output->bufferSize();
@@ -193,6 +192,7 @@ bool EglGbmBackend::resetOutput(Output &output)
     cleanupRenderData(output.old);
     output.old = output.current;
     output.current = {};
+    output.current.format = gbmFormat.value();
     output.current.gbmSurface = gbmSurface;
 
     if (!output.output->needsSoftwareTransformation())  {
@@ -349,6 +349,7 @@ QSharedPointer<DrmBuffer> EglGbmBackend::importFramebuffer(Output &output, const
     qCWarning(KWIN_DRM) << "all imports failed on output" << output.output;
     // try again with XRGB8888, the most universally supported basic format
     output.forceXrgb8888 = true;
+    renderingBackend()->setForceXrgb8888(output.output);
     return nullptr;
 }
 
@@ -860,6 +861,11 @@ bool EglGbmBackend::prefer10bpc() const
     static bool ok = false;
     static const int preferred = qEnvironmentVariableIntValue("KWIN_DRM_PREFER_COLOR_DEPTH", &ok);
     return !ok || preferred == 30;
+}
+
+void EglGbmBackend::setForceXrgb8888(DrmAbstractOutput *output) {
+    auto &o = m_outputs[output];
+    o.forceXrgb8888 = true;
 }
 
 bool operator==(const GbmFormat &lhs, const GbmFormat &rhs)

src/backends/drm/egl_gbm_backend.h
@@ -141,6 +141,8 @@ private:
 
     static EglGbmBackend *renderingBackend();
 
+    void setForceXrgb8888(DrmAbstractOutput *output);
+
     friend class EglGbmTexture;
 };
 