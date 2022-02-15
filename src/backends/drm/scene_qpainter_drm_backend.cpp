Kleysley/kwin

backends/drm: move damage tracking into DumbSwapchain

src/backends/drm/scene_qpainter_drm_backend.cpp
@@ -30,61 +30,39 @@ DrmQPainterBackend::DrmQPainterBackend(DrmBackend *backend, DrmGpu *gpu)
     connect(m_gpu, &DrmGpu::outputEnabled, this, &DrmQPainterBackend::initOutput);
     connect(m_gpu, &DrmGpu::outputDisabled, this,
         [this] (DrmAbstractOutput *o) {
-            auto it = std::find_if(m_outputs.begin(), m_outputs.end(),
-                [o] (const Output &output) {
-                    return output.output == o;
-                }
-            );
-            if (it == m_outputs.end()) {
-                return;
-            }
-            m_outputs.erase(it);
+            m_swapchains.remove(o);
         }
     );
 }
 
 void DrmQPainterBackend::initOutput(DrmAbstractOutput *output)
 {
-    Output o;
-    o.swapchain = QSharedPointer<DumbSwapchain>::create(m_gpu, output->pixelSize(), DRM_FORMAT_XRGB8888);
-    o.output = output;
-    m_outputs.insert(output, o);
+    m_swapchains.insert(output, QSharedPointer<DumbSwapchain>::create(m_gpu, output->pixelSize(), DRM_FORMAT_XRGB8888));
     connect(output, &DrmOutput::currentModeChanged, this,
         [output, this] {
-            auto &o = m_outputs[output];
-            o.swapchain = QSharedPointer<DumbSwapchain>::create(m_gpu, output->pixelSize(), DRM_FORMAT_XRGB8888);
-            o.damageJournal.setCapacity(o.swapchain->slotCount());
+            m_swapchains[output] = QSharedPointer<DumbSwapchain>::create(m_gpu, output->pixelSize(), DRM_FORMAT_XRGB8888);
         }
     );
 }
 
 QImage *DrmQPainterBackend::bufferForScreen(AbstractOutput *output)
 {
-    return m_outputs[output].swapchain->currentBuffer()->image();
+    return m_swapchains[output]->currentBuffer()->image();
 }
 
 QRegion DrmQPainterBackend::beginFrame(AbstractOutput *output)
 {
-    Output *rendererOutput = &m_outputs[output];
-
-    int bufferAge;
-    rendererOutput->swapchain->acquireBuffer(&bufferAge);
-
-    return rendererOutput->damageJournal.accumulate(bufferAge, rendererOutput->output->geometry());
+    QRegion needsRepainting;
+    m_swapchains[output]->acquireBuffer(output->geometry(), &needsRepainting);
+    return needsRepainting;
 }
 
 void DrmQPainterBackend::endFrame(AbstractOutput *output, const QRegion &renderedRegion, const QRegion &damage)
 {
     Q_UNUSED(renderedRegion)
-    Output &rendererOutput = m_outputs[output];
-    DrmAbstractOutput *drmOutput = rendererOutput.output;
-
-    QSharedPointer<DrmDumbBuffer> back = rendererOutput.swapchain->currentBuffer();
-    rendererOutput.swapchain->releaseBuffer(back);
-
-    drmOutput->present(back, drmOutput->geometry());
-
-    rendererOutput.damageJournal.add(damage);
+    QSharedPointer<DrmDumbBuffer> back = m_swapchains[output]->currentBuffer();
+    m_swapchains[output]->releaseBuffer(back, damage);
+    static_cast<DrmAbstractOutput*>(output)->present(back, output->geometry());
 }
 
 }

arkroyal01/volcanic

src/backends/drm/scene_qpainter_drm_backend.cpp
@@ -30,61 +30,39 @@ DrmQPainterBackend::DrmQPainterBackend(DrmBackend *backend, DrmGpu *gpu)
     connect(m_gpu, &DrmGpu::outputEnabled, this, &DrmQPainterBackend::initOutput);
     connect(m_gpu, &DrmGpu::outputDisabled, this,
         [this] (DrmAbstractOutput *o) {
-            auto it = std::find_if(m_outputs.begin(), m_outputs.end(),
-                [o] (const Output &output) {
-                    return output.output == o;
-                }
-            );
-            if (it == m_outputs.end()) {
-                return;
-            }
-            m_outputs.erase(it);
+            m_swapchains.remove(o);
         }
     );
 }
 
 void DrmQPainterBackend::initOutput(DrmAbstractOutput *output)
 {
-    Output o;
-    o.swapchain = QSharedPointer<DumbSwapchain>::create(m_gpu, output->sourceSize(), DRM_FORMAT_XRGB8888);
-    o.output = output;
-    m_outputs.insert(output, o);
+    m_swapchains.insert(output, QSharedPointer<DumbSwapchain>::create(m_gpu, output->sourceSize(), DRM_FORMAT_XRGB8888));
     connect(output, &DrmOutput::currentModeChanged, this,
         [output, this] {
-            auto &o = m_outputs[output];
-            o.swapchain = QSharedPointer<DumbSwapchain>::create(m_gpu, output->sourceSize(), DRM_FORMAT_XRGB8888);
-            o.damageJournal.setCapacity(o.swapchain->slotCount());
+            m_swapchains[output] = QSharedPointer<DumbSwapchain>::create(m_gpu, output->sourceSize(), DRM_FORMAT_XRGB8888);
         }
     );
 }
 
 QImage *DrmQPainterBackend::bufferForScreen(AbstractOutput *output)
 {
-    return m_outputs[output].swapchain->currentBuffer()->image();
+    return m_swapchains[output]->currentBuffer()->image();
 }
 
 QRegion DrmQPainterBackend::beginFrame(AbstractOutput *output)
 {
-    Output *rendererOutput = &m_outputs[output];
-
-    int bufferAge;
-    rendererOutput->swapchain->acquireBuffer(&bufferAge);
-
-    return rendererOutput->damageJournal.accumulate(bufferAge, rendererOutput->output->geometry());
+    QRegion needsRepainting;
+    m_swapchains[output]->acquireBuffer(output->geometry(), &needsRepainting);
+    return needsRepainting;
 }
 
 void DrmQPainterBackend::endFrame(AbstractOutput *output, const QRegion &renderedRegion, const QRegion &damage)
 {
     Q_UNUSED(renderedRegion)
-    Output &rendererOutput = m_outputs[output];
-    DrmAbstractOutput *drmOutput = rendererOutput.output;
-
-    QSharedPointer<DrmDumbBuffer> back = rendererOutput.swapchain->currentBuffer();
-    rendererOutput.swapchain->releaseBuffer(back);
-
-    drmOutput->present(back, drmOutput->geometry());
-
-    rendererOutput.damageJournal.add(damage);
+    QSharedPointer<DrmDumbBuffer> back = m_swapchains[output]->currentBuffer();
+    m_swapchains[output]->releaseBuffer(back, damage);
+    static_cast<DrmAbstractOutput*>(output)->present(back, output->geometry());
 }
 
 }