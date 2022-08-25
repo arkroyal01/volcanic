rosalinux/kwin

Port Workspace to Platform::{outputAdded,outputDisabled} signals

src/workspace.cpp
@@ -207,14 +207,12 @@ void Workspace::init()
     m_focusChain->setSeparateScreenFocus(options->isSeparateScreenFocus());
 
     Platform *platform = kwinApp()->platform();
-    connect(platform, &Platform::outputEnabled, this, &Workspace::slotOutputEnabled);
-    connect(platform, &Platform::outputDisabled, this, &Workspace::slotOutputDisabled);
+    connect(platform, &Platform::outputAdded, this, &Workspace::slotOutputAdded);
+    connect(platform, &Platform::outputRemoved, this, &Workspace::slotOutputRemoved);
 
     const QVector<Output *> outputs = platform->outputs();
     for (Output *output : outputs) {
-        if (output->isEnabled()) {
-            slotOutputEnabled(output);
-        }
+        slotOutputAdded(output);
     }
 
     Screens *screens = Screens::self();
@@ -1203,11 +1201,34 @@ Output *Workspace::outputAt(const QPointF &pos) const
     return bestOutput;
 }
 
-void Workspace::slotOutputEnabled(Output *output)
+void Workspace::slotOutputAdded(Output *output)
 {
     if (output->isNonDesktop()) {
         return;
     }
+
+    if (output->isEnabled()) {
+        addOutput(output);
+    }
+
+    connect(output, &Output::enabledChanged, this, [this, output]() {
+        if (output->isEnabled()) {
+            addOutput(output);
+        } else {
+            removeOutput(output);
+        }
+    });
+}
+
+void Workspace::slotOutputRemoved(Output *output)
+{
+    if (!output->isNonDesktop()) {
+        removeOutput(output);
+    }
+}
+
+void Workspace::addOutput(Output *output)
+{
     if (!m_activeOutput) {
         m_activeOutput = output;
     }
@@ -1228,13 +1249,11 @@ void Workspace::slotOutputEnabled(Output *output)
     Q_EMIT outputAdded(output);
 }
 
-void Workspace::slotOutputDisabled(Output *output)
+void Workspace::removeOutput(Output *output)
 {
-    if (output->isNonDesktop()) {
+    if (!m_outputs.removeOne(output)) {
         return;
     }
-
-    m_outputs.removeOne(output);
 
     if (m_activeOutput == output) {
         m_activeOutput = outputAt(output->geometry().center());

src/workspace.h
@@ -522,8 +522,8 @@ private Q_SLOTS:
     void slotCurrentDesktopChangingCancelled();
     void slotDesktopAdded(VirtualDesktop *desktop);
     void slotDesktopRemoved(VirtualDesktop *desktop);
-    void slotOutputEnabled(Output *output);
-    void slotOutputDisabled(Output *output);
+    void slotOutputAdded(Output *output);
+    void slotOutputRemoved(Output *output);
 
 Q_SIGNALS:
     /**
@@ -601,6 +601,9 @@ private:
     void setupWindowConnections(Window *window);
     Unmanaged *createUnmanaged(xcb_window_t windowId);
     void addUnmanaged(Unmanaged *c);
+
+    void addOutput(Output *output);
+    void removeOutput(Output *output);
 
     void addWaylandWindow(Window *window);
     void removeWaylandWindow(Window *window);

arkroyal01/volcanic

src/workspace.cpp
@@ -213,14 +213,12 @@ void Workspace::init()
     }
 
     Platform *platform = kwinApp()->platform();
-    connect(platform, &Platform::outputEnabled, this, &Workspace::slotOutputEnabled);
-    connect(platform, &Platform::outputDisabled, this, &Workspace::slotOutputDisabled);
+    connect(platform, &Platform::outputAdded, this, &Workspace::slotPlatformOutputAdded);
+    connect(platform, &Platform::outputRemoved, this, &Workspace::slotPlatformOutputRemoved);
 
     const QVector<Output *> outputs = platform->outputs();
     for (Output *output : outputs) {
-        if (output->isEnabled()) {
-            slotOutputEnabled(output);
-        }
+        slotPlatformOutputAdded(output);
     }
 
     m_screens->init();
@@ -1380,11 +1378,34 @@ Output *Workspace::outputAt(const QPointF &pos) const
     return bestOutput;
 }
 
-void Workspace::slotOutputEnabled(Output *output)
+void Workspace::slotPlatformOutputAdded(Output *output)
 {
     if (output->isNonDesktop()) {
         return;
     }
+
+    if (output->isEnabled()) {
+        addOutput(output);
+    }
+
+    connect(output, &Output::enabledChanged, this, [this, output]() {
+        if (output->isEnabled()) {
+            addOutput(output);
+        } else {
+            removeOutput(output);
+        }
+    });
+}
+
+void Workspace::slotPlatformOutputRemoved(Output *output)
+{
+    if (!output->isNonDesktop()) {
+        removeOutput(output);
+    }
+}
+
+void Workspace::addOutput(Output *output)
+{
     if (!m_activeOutput) {
         m_activeOutput = output;
     }
@@ -1405,13 +1426,11 @@ void Workspace::slotOutputEnabled(Output *output)
     Q_EMIT outputAdded(output);
 }
 
-void Workspace::slotOutputDisabled(Output *output)
+void Workspace::removeOutput(Output *output)
 {
-    if (output->isNonDesktop()) {
+    if (!m_outputs.removeOne(output)) {
         return;
     }
-
-    m_outputs.removeOne(output);
 
     if (m_activeOutput == output) {
         m_activeOutput = outputAt(output->geometry().center());

src/workspace.h
@@ -539,8 +539,8 @@ private Q_SLOTS:
     void slotCurrentDesktopChangingCancelled();
     void slotDesktopAdded(VirtualDesktop *desktop);
     void slotDesktopRemoved(VirtualDesktop *desktop);
-    void slotOutputEnabled(Output *output);
-    void slotOutputDisabled(Output *output);
+    void slotPlatformOutputAdded(Output *output);
+    void slotPlatformOutputRemoved(Output *output);
 
 Q_SIGNALS:
     /**
@@ -618,6 +618,9 @@ private:
     void setupWindowConnections(Window *window);
     Unmanaged *createUnmanaged(xcb_window_t windowId);
     void addUnmanaged(Unmanaged *c);
+
+    void addOutput(Output *output);
+    void removeOutput(Output *output);
 
     void addWaylandWindow(Window *window);
     void removeWaylandWindow(Window *window);