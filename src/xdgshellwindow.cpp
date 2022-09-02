rosalinux/kwin

wayland: Use correct output when sending xdg_toplevel.configure_bounds
We should use the next output rather than the current output.

src/xdgshellwindow.cpp
@@ -866,7 +866,7 @@ XdgSurfaceConfigure *XdgToplevelWindow::sendRoleConfigure() const
     }
 
     if (nextClientSize.isEmpty()) {
-        QSize bounds = workspace()->clientArea(PlacementArea, this, output()).size();
+        QSize bounds = workspace()->clientArea(PlacementArea, this, moveResizeOutput()).size();
         bounds.rwidth() -= framePadding.width();
         bounds.rheight() -= framePadding.height();
         m_shellSurface->sendConfigureBounds(bounds);

arkroyal01/volcanic

src/xdgshellwindow.cpp
@@ -865,7 +865,7 @@ XdgSurfaceConfigure *XdgToplevelWindow::sendRoleConfigure() const
     }
 
     if (nextClientSize.isEmpty()) {
-        QSizeF bounds = workspace()->clientArea(PlacementArea, this, output()).size();
+        QSizeF bounds = workspace()->clientArea(PlacementArea, this, moveResizeOutput()).size();
         bounds.rwidth() -= framePadding.width();
         bounds.rheight() -= framePadding.height();
         m_shellSurface->sendConfigureBounds(bounds.toSize());