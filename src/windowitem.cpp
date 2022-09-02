rosalinux/kwin

Make it possible to raise windows on top of the lockscreen
Requires clients to have the
X-KDE-Wayland-Interfaces=kde_lockscreenallowed_v1 set in their desktop
file, then they will be able to use the kde_lockscreenallowed_v1
protocol to raise any surface above the lockscreen.
The protocol has only 1 method, raise_surface to do exactly that.

Makes it possible to implement
https://invent.kde.org/teams/plasma-mobile/issues/-/issues/98

src/windowitem.cpp
@@ -35,6 +35,7 @@ WindowItem::WindowItem(Window *window, Item *parent)
     if (waylandServer()) {
         connect(waylandServer(), &WaylandServer::lockStateChanged, this, &WindowItem::updateVisibility);
     }
+    connect(window, &Window::aboveLockScreenChanged, this, &WindowItem::updateVisibility);
     connect(window, &Window::minimizedChanged, this, &WindowItem::updateVisibility);
     connect(window, &Window::hiddenChanged, this, &WindowItem::updateVisibility);
     connect(window, &Window::activitiesChanged, this, &WindowItem::updateVisibility);
@@ -127,7 +128,7 @@ void WindowItem::handleWindowClosed(Window *original, Deleted *deleted)
 bool WindowItem::computeVisibility() const
 {
     if (waylandServer() && waylandServer()->isScreenLocked()) {
-        return m_window->isLockScreen() || m_window->isInputMethod();
+        return m_window->isLockScreen() || m_window->isInputMethod() || m_window->isLockScreenOverlay();
     }
     if (m_window->isDeleted()) {
         if (m_forceVisibleByDeleteCount == 0) {

arkroyal01/volcanic

src/windowitem.cpp
@@ -35,6 +35,7 @@ WindowItem::WindowItem(Window *window, Item *parent)
     if (waylandServer()) {
         connect(waylandServer(), &WaylandServer::lockStateChanged, this, &WindowItem::updateVisibility);
     }
+    connect(window, &Window::lockScreenOverlayChanged, this, &WindowItem::updateVisibility);
     connect(window, &Window::minimizedChanged, this, &WindowItem::updateVisibility);
     connect(window, &Window::hiddenChanged, this, &WindowItem::updateVisibility);
     connect(window, &Window::activitiesChanged, this, &WindowItem::updateVisibility);
@@ -127,7 +128,7 @@ void WindowItem::handleWindowClosed(Window *original, Deleted *deleted)
 bool WindowItem::computeVisibility() const
 {
     if (waylandServer() && waylandServer()->isScreenLocked()) {
-        return m_window->isLockScreen() || m_window->isInputMethod();
+        return m_window->isLockScreen() || m_window->isInputMethod() || m_window->isLockScreenOverlay();
     }
     if (m_window->isDeleted()) {
         if (m_forceVisibleByDeleteCount == 0) {