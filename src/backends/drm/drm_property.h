rosalinux/kwin

backends/drm: fixes for bitmask drm properties

src/backends/drm/drm_property.h
@@ -3,13 +3,14 @@
     This file is part of the KDE project.
 
     SPDX-FileCopyrightText: 2016 Roman Gilg <subdiff@gmail.com>
-    SPDX-FileCopyrightText: 2021 Xaver Hugl <xaver.hugl@gmail.com>
+    SPDX-FileCopyrightText: 2021-2022 Xaver Hugl <xaver.hugl@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #pragma once
 #include "drm_pointer.h"
+#include "logging.h"
 
 #include <QByteArray>
 #include <QMap>
@@ -26,31 +27,50 @@ class DrmProperty
 {
 public:
     DrmProperty(DrmObject *obj, drmModePropertyRes *prop, uint64_t val, const QVector<QByteArray> &enumNames);
-    virtual ~DrmProperty();
 
-    void initEnumMap(drmModePropertyRes *prop);
-
-    QVector<QByteArray> enumNames() const;
-    bool hasEnum(uint64_t value) const;
     bool hasAllEnums() const;
+
+    template<typename Enum>
+    bool hasEnum(Enum value) const
+    {
+        return m_enumToPropertyMap.contains(static_cast<uint64_t>(value));
+    }
     template<typename T>
     T enumForValue(uint64_t value) const
     {
-        return static_cast<T>(m_enumMap[value]);
-    }
-    template<typename T>
-    bool setEnum(T index)
-    {
-        if (hasEnum(static_cast<uint64_t>(index))) {
-            setPending(m_enumMap[static_cast<uint32_t>(index)]);
-            return true;
+        if (m_isBitmask) {
+            uint64_t ret = 0;
+            for (uint64_t mask = 1; value >= mask && mask != 0; mask <<= 1) {
+                if (value & mask) {
+                    ret |= m_propertyToEnumMap[mask];
+                }
+            }
+            return static_cast<T>(ret);
+        } else {
+            return static_cast<T>(m_propertyToEnumMap[value]);
         }
-        return false;
+    }
+    template<typename Enum>
+    void setEnum(Enum value)
+    {
+        const uint64_t integer = static_cast<uint64_t>(value);
+        if (m_isBitmask) {
+            uint64_t set = 0;
+            for (uint64_t mask = 1; integer >= mask && mask != 0; mask <<= 1) {
+                if (integer & mask) {
+                    set |= m_enumToPropertyMap[mask];
+                }
+            }
+            setPending(set);
+        } else {
+            setPending(m_enumToPropertyMap[integer]);
+        }
     }
 
     uint32_t propId() const;
     const QByteArray &name() const;
     bool isImmutable() const;
+    bool isBitmask() const;
     bool isLegacy() const;
     /**
      * Makes this property be ignored by DrmObject::atomicPopulate
@@ -77,12 +97,15 @@ public:
     bool setEnumLegacy(T value)
     {
         if (hasEnum(static_cast<uint64_t>(value))) {
-            return setPropertyLegacy(m_enumMap[static_cast<uint32_t>(value)]);
+            return setPropertyLegacy(m_enumToPropertyMap[static_cast<uint32_t>(value)]);
         }
         return false;
     }
 
+    QString valueString(uint64_t value) const;
+
 private:
+    void initEnumMap(drmModePropertyRes *prop);
     void updateBlob();
 
     uint32_t m_propId = 0;
@@ -101,10 +124,12 @@ private:
     uint64_t m_minValue = -1;
     uint64_t m_maxValue = -1;
 
-    QMap<uint32_t, uint64_t> m_enumMap;
+    QMap<uint64_t, uint64_t> m_enumToPropertyMap;
+    QMap<uint64_t, uint64_t> m_propertyToEnumMap;
     QVector<QByteArray> m_enumNames;
     const bool m_immutable;
     const bool m_isBlob;
+    const bool m_isBitmask;
     bool m_legacy = false;
     const DrmObject *m_obj;
 };

arkroyal01/volcanic

src/backends/drm/drm_property.h
@@ -3,13 +3,14 @@
     This file is part of the KDE project.
 
     SPDX-FileCopyrightText: 2016 Roman Gilg <subdiff@gmail.com>
-    SPDX-FileCopyrightText: 2021 Xaver Hugl <xaver.hugl@gmail.com>
+    SPDX-FileCopyrightText: 2021-2022 Xaver Hugl <xaver.hugl@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #pragma once
 #include "drm_pointer.h"
+#include "drm_logging.h"
 
 #include <QByteArray>
 #include <QMap>
@@ -26,31 +27,50 @@ class DrmProperty
 {
 public:
     DrmProperty(DrmObject *obj, drmModePropertyRes *prop, uint64_t val, const QVector<QByteArray> &enumNames);
-    virtual ~DrmProperty();
 
-    void initEnumMap(drmModePropertyRes *prop);
-
-    QVector<QByteArray> enumNames() const;
-    bool hasEnum(uint64_t value) const;
     bool hasAllEnums() const;
+
+    template<typename Enum>
+    bool hasEnum(Enum value) const
+    {
+        return m_enumToPropertyMap.contains(static_cast<uint64_t>(value));
+    }
     template<typename T>
     T enumForValue(uint64_t value) const
     {
-        return static_cast<T>(m_enumMap[value]);
-    }
-    template<typename T>
-    bool setEnum(T index)
-    {
-        if (hasEnum(static_cast<uint64_t>(index))) {
-            setPending(m_enumMap[static_cast<uint32_t>(index)]);
-            return true;
+        if (m_isBitmask) {
+            uint64_t ret = 0;
+            for (uint64_t mask = 1; value >= mask && mask != 0; mask <<= 1) {
+                if (value & mask) {
+                    ret |= m_propertyToEnumMap[mask];
+                }
+            }
+            return static_cast<T>(ret);
+        } else {
+            return static_cast<T>(m_propertyToEnumMap[value]);
         }
-        return false;
+    }
+    template<typename Enum>
+    void setEnum(Enum value)
+    {
+        const uint64_t integer = static_cast<uint64_t>(value);
+        if (m_isBitmask) {
+            uint64_t set = 0;
+            for (uint64_t mask = 1; integer >= mask && mask != 0; mask <<= 1) {
+                if (integer & mask) {
+                    set |= m_enumToPropertyMap[mask];
+                }
+            }
+            setPending(set);
+        } else {
+            setPending(m_enumToPropertyMap[integer]);
+        }
     }
 
     uint32_t propId() const;
     const QByteArray &name() const;
     bool isImmutable() const;
+    bool isBitmask() const;
     bool isLegacy() const;
     /**
      * Makes this property be ignored by DrmObject::atomicPopulate
@@ -77,12 +97,15 @@ public:
     bool setEnumLegacy(T value)
     {
         if (hasEnum(static_cast<uint64_t>(value))) {
-            return setPropertyLegacy(m_enumMap[static_cast<uint32_t>(value)]);
+            return setPropertyLegacy(m_enumToPropertyMap[static_cast<uint32_t>(value)]);
         }
         return false;
     }
 
+    QString valueString(uint64_t value) const;
+
 private:
+    void initEnumMap(drmModePropertyRes *prop);
     void updateBlob();
 
     uint32_t m_propId = 0;
@@ -101,10 +124,12 @@ private:
     uint64_t m_minValue = -1;
     uint64_t m_maxValue = -1;
 
-    QMap<uint32_t, uint64_t> m_enumMap;
+    QMap<uint64_t, uint64_t> m_enumToPropertyMap;
+    QMap<uint64_t, uint64_t> m_propertyToEnumMap;
     QVector<QByteArray> m_enumNames;
     const bool m_immutable;
     const bool m_isBlob;
+    const bool m_isBitmask;
     bool m_legacy = false;
     const DrmObject *m_obj;
 };