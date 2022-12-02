KDE/kwin

placement: cascade if window would completely overlap another window

src/placement.h
@@ -37,6 +37,8 @@ public:
 
     void reinitCascading(int desktop);
 
+    void cascadeIfOverlapping(Window *c, const QRectF &area);
+
     /**
      * Cascades all clients on the current desktop
      */

arkroyal01/volcanic

BUG: 58063

src/placement.h
@@ -37,6 +37,8 @@ public:
 
     void reinitCascading(int desktop);
 
+    void cascadeIfCovering(Window *c, const QRectF &area);
+
     /**
      * Cascades all clients on the current desktop
      */