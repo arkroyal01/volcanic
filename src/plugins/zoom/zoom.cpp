KDE/kwin

libkwineffects: handle GLTexture allocation failures
This is required for properly dealing with GPU resets

src/plugins/zoom/zoom.cpp
@@ -265,7 +265,10 @@ ZoomEffect::OffscreenData *ZoomEffect::ensureOffscreenData(const RenderViewport 
     data.viewport = rect;
 
     if (!data.texture || data.texture->size() != nativeSize) {
-        data.texture.reset(new GLTexture(GL_RGBA8, nativeSize));
+        data.texture = GLTexture::allocate(GL_RGBA8, nativeSize);
+        if (!data.texture) {
+            return nullptr;
+        }
         data.texture->setFilter(GL_LINEAR);
         data.texture->setWrapMode(GL_CLAMP_TO_EDGE);
         data.framebuffer = std::make_unique<GLFramebuffer>(data.texture.get());
@@ -277,6 +280,9 @@ ZoomEffect::OffscreenData *ZoomEffect::ensureOffscreenData(const RenderViewport 
 void ZoomEffect::paintScreen(const RenderTarget &renderTarget, const RenderViewport &viewport, int mask, const QRegion &region, EffectScreen *screen)
 {
     OffscreenData *offscreenData = ensureOffscreenData(viewport, screen);
+    if (!offscreenData) {
+        return;
+    }
 
     // Render the scene in an offscreen texture and then upscale it.
     RenderTarget offscreenRenderTarget(offscreenData->framebuffer.get());

arkroyal01/volcanic

src/plugins/zoom/zoom.cpp
@@ -266,7 +266,10 @@ ZoomEffect::OffscreenData *ZoomEffect::ensureOffscreenData(const RenderTarget &r
 
     const GLenum textureFormat = renderTarget.colorDescription() == ColorDescription::sRGB ? GL_RGBA8 : GL_RGBA16F;
     if (!data.texture || data.texture->size() != nativeSize || data.texture->internalFormat() != textureFormat) {
-        data.texture.reset(new GLTexture(textureFormat, nativeSize));
+        data.texture = GLTexture::allocate(textureFormat, nativeSize);
+        if (!data.texture) {
+            return nullptr;
+        }
         data.texture->setFilter(GL_LINEAR);
         data.texture->setWrapMode(GL_CLAMP_TO_EDGE);
         data.framebuffer = std::make_unique<GLFramebuffer>(data.texture.get());
@@ -278,6 +281,9 @@ ZoomEffect::OffscreenData *ZoomEffect::ensureOffscreenData(const RenderTarget &r
 void ZoomEffect::paintScreen(const RenderTarget &renderTarget, const RenderViewport &viewport, int mask, const QRegion &region, EffectScreen *screen)
 {
     OffscreenData *offscreenData = ensureOffscreenData(renderTarget, viewport, screen);
+    if (!offscreenData) {
+        return;
+    }
 
     // Render the scene in an offscreen texture and then upscale it.
     RenderTarget offscreenRenderTarget(offscreenData->framebuffer.get(), renderTarget.colorDescription());