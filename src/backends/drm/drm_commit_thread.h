hasan-pisi/kwin

backends/drm: offload atomic commits to a separate thread
This separate thread submits commits as late as possible, so that until
immediately before vblank the cursor position can still be updated, reducing
its latency and opening the door for more optimizations.

src/backends/drm/drm_commit_thread.h
@@ -1,0 +1,43 @@
+/*
+    KWin - the KDE window manager
+    This file is part of the KDE project.
+
+    SPDX-FileCopyrightText: 2023 Xaver Hugl <xaver.hugl@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+#include <QThread>
+#include <condition_variable>
+#include <mutex>
+
+namespace KWin
+{
+
+class DrmGpu;
+class DrmAtomicCommit;
+
+class DrmCommitThread : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DrmCommitThread();
+    ~DrmCommitThread();
+
+    void setCommit(std::unique_ptr<DrmAtomicCommit> &&commit, std::chrono::nanoseconds targetCommitTime);
+    bool replaceCommit(std::unique_ptr<DrmAtomicCommit> &&commit);
+
+Q_SIGNALS:
+    void commitFailed();
+
+private:
+    std::unique_ptr<DrmAtomicCommit> m_commit;
+    std::unique_ptr<QThread> m_thread;
+    std::mutex m_mutex;
+    std::condition_variable m_commitPending;
+    std::chrono::nanoseconds m_targetCommitTime;
+};
+
+}

arkroyal01/volcanic

src/backends/drm/drm_commit_thread.h
@@ -1,0 +1,43 @@
+/*
+    KWin - the KDE window manager
+    This file is part of the KDE project.
+
+    SPDX-FileCopyrightText: 2023 Xaver Hugl <xaver.hugl@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+#include <QThread>
+#include <condition_variable>
+#include <mutex>
+
+namespace KWin
+{
+
+class DrmGpu;
+class DrmAtomicCommit;
+
+class DrmCommitThread : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DrmCommitThread();
+    ~DrmCommitThread();
+
+    void setCommit(std::unique_ptr<DrmAtomicCommit> &&commit, std::chrono::nanoseconds targetPageflipTime);
+    bool replaceCommit(std::unique_ptr<DrmAtomicCommit> &&commit);
+
+Q_SIGNALS:
+    void commitFailed();
+
+private:
+    std::unique_ptr<DrmAtomicCommit> m_commit;
+    std::unique_ptr<QThread> m_thread;
+    std::mutex m_mutex;
+    std::condition_variable m_commitPending;
+    std::chrono::nanoseconds m_targetPageflipTime;
+};
+
+}