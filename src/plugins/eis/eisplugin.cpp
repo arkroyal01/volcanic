KDE/kwin

Add a libeis input backend plugin
This adds a libeis backend via plugin which supports clients sending
emulated input events. No public listening socket is exposed,
clients are expected to go through the RemoteDesktop portal.
In order to restrict the device types available to clients
according to what was approved via the portal a separate eis
context per portal request is created. The communication with
the portal happens through a simple dbus interface where cookies
are  handed out for each eis context so the portal can inform KWin
when the portal session is closed/should end.

src/plugins/eis/eisplugin.cpp
@@ -1,0 +1,15 @@
+#include "eisplugin.h"
+
+#include "inputbackend.h"
+
+#include "input.h"
+
+EisPlugin::EisPlugin()
+    : Plugin()
+{
+    KWin::input()->addInputBackend(std::make_unique<KWin::EisBackend>());
+}
+
+EisPlugin::~EisPlugin()
+{
+}

arkroyal01/volcanic

src/plugins/eis/eisplugin.cpp
@@ -1,0 +1,15 @@
+#include "eisplugin.h"
+
+#include "eisbackend.h"
+
+#include "input.h"
+
+EisPlugin::EisPlugin()
+    : Plugin()
+{
+    KWin::input()->addInputBackend(std::make_unique<KWin::EisBackend>());
+}
+
+EisPlugin::~EisPlugin()
+{
+}