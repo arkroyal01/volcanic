DawfukFR/kwin

backends: refactor direct scanout and dmabuf feedback
This moves some of the responsbilities up in the stack, which simplifies
the backends and opens up some future possibilities like making direct scanout
work for non-surface items

src/core/outputlayer.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "outputlayer.h"
+#include "scene/surfaceitem.h"
 
 namespace KWin
 {
@@ -69,9 +70,47 @@ bool OutputLayer::needsRepaint() const
     return !m_repaints.isEmpty();
 }
 
-bool OutputLayer::scanout(SurfaceItem *surfaceItem)
+bool OutputLayer::doAttemptScanout(GraphicsBuffer *buffer, const QRectF &sourceRect, const QSizeF &targetSize, OutputTransform transform, const ColorDescription &color, const QRegion &damage)
 {
     return false;
+}
+
+bool OutputLayer::attemptScanout(Item *item)
+{
+    const auto surfaceItem = qobject_cast<SurfaceItem *>(item);
+    if (!surfaceItem || !surfaceItem->pixmap() || !surfaceItem->pixmap()->buffer()) {
+        return false;
+    }
+    const auto buffer = surfaceItem->pixmap()->buffer();
+    const auto attrs = buffer->dmabufAttributes();
+    if (!attrs) {
+        return false;
+    }
+    const auto formats = supportedDrmFormats();
+    if (!formats.contains(attrs->format) || !formats[attrs->format].contains(attrs->modifier)) {
+        if (m_scanoutCandidate && m_scanoutCandidate != item) {
+            m_scanoutCandidate->setScanoutHint(nullptr, {});
+        }
+        m_scanoutCandidate = item;
+        item->setScanoutHint(scanoutDevice(), supportedDrmFormats());
+        return false;
+    }
+    // TODO actually do proper damage tracking
+    const bool ret = doAttemptScanout(buffer, surfaceItem->bufferSourceBox(), surfaceItem->destinationSize(), surfaceItem->bufferTransform(), item->colorDescription(), infiniteRegion());
+    if (ret) {
+        surfaceItem->resetDamage();
+        // ensure the pixmap is updated when direct scanout ends
+        surfaceItem->destroyPixmap();
+    }
+    return ret;
+}
+
+void OutputLayer::notifyNoScanoutCandidate()
+{
+    if (m_scanoutCandidate) {
+        m_scanoutCandidate->setScanoutHint(nullptr, {});
+        m_scanoutCandidate = nullptr;
+    }
 }
 
 void OutputLayer::setPosition(const QPointF &position)

src/core/outputlayer.h
@@ -10,6 +10,7 @@
 #include "kwin_export.h"
 
 #include <QObject>
+#include <QPointer>
 #include <QRegion>
 #include <chrono>
 #include <optional>
@@ -18,6 +19,8 @@ namespace KWin
 {
 
 class SurfaceItem;
+class DrmDevice;
+class GraphicsBuffer;
 
 struct OutputLayerBeginFrameInfo
 {
@@ -69,20 +72,31 @@ public:
      * Tries to import the newest buffer of the surface for direct scanout
      * Returns @c true if scanout succeeds, @c false if rendering is necessary
      */
-    virtual bool scanout(SurfaceItem *surfaceItem);
+    bool attemptScanout(Item *item);
+
+    /**
+     * Notify that there's no scanout candidate this frame
+     */
+    void notifyNoScanoutCandidate();
 
     /**
      * queries the render time of the last frame. If rendering isn't complete yet, this may block until it is
      */
     virtual std::chrono::nanoseconds queryRenderTime() const = 0;
 
-private:
+    virtual DrmDevice *scanoutDevice() const = 0;
+    virtual QHash<uint32_t, QList<uint64_t>> supportedDrmFormats() const = 0;
+
+protected:
+    virtual bool doAttemptScanout(GraphicsBuffer *buffer, const QRectF &sourceRect, const QSizeF &targetSize, OutputTransform transform, const ColorDescription &color, const QRegion &damage);
+
     QRegion m_repaints;
     QPointF m_hotspot;
     QPointF m_position;
     QSizeF m_size;
     qreal m_scale = 1.0;
     bool m_enabled = false;
+    QPointer<Item> m_scanoutCandidate;
 };
 
 } // namespace KWin

arkroyal01/volcanic

src/core/outputlayer.cpp
@@ -5,6 +5,9 @@
 */
 
 #include "outputlayer.h"
+#include "scene/surfaceitem.h"
+#include "scene/surfaceitem_wayland.h"
+#include "wayland/surface.h"
 
 namespace KWin
 {
@@ -69,9 +72,47 @@ bool OutputLayer::needsRepaint() const
     return !m_repaints.isEmpty();
 }
 
-bool OutputLayer::scanout(SurfaceItem *surfaceItem)
+bool OutputLayer::doAttemptScanout(GraphicsBuffer *buffer, const QRectF &sourceRect, const QSizeF &targetSize, OutputTransform transform, const ColorDescription &color, const QRegion &damage)
 {
     return false;
+}
+
+bool OutputLayer::attemptScanout(SurfaceItem *surfaceItem)
+{
+    SurfaceItemWayland *wayland = qobject_cast<SurfaceItemWayland *>(surfaceItem);
+    if (!wayland || !wayland->surface()) {
+        return false;
+    }
+    const auto buffer = wayland->surface()->buffer();
+    const auto attrs = buffer->dmabufAttributes();
+    if (!attrs) {
+        return false;
+    }
+    const auto formats = supportedDrmFormats();
+    if (!formats.contains(attrs->format) || !formats[attrs->format].contains(attrs->modifier)) {
+        if (m_scanoutCandidate && m_scanoutCandidate != surfaceItem) {
+            m_scanoutCandidate->setScanoutHint(nullptr, {});
+        }
+        m_scanoutCandidate = surfaceItem;
+        surfaceItem->setScanoutHint(scanoutDevice(), supportedDrmFormats());
+        return false;
+    }
+    // TODO actually do proper damage tracking
+    const bool ret = doAttemptScanout(buffer, surfaceItem->bufferSourceBox(), surfaceItem->destinationSize(), surfaceItem->bufferTransform(), surfaceItem->colorDescription(), infiniteRegion());
+    if (ret) {
+        surfaceItem->resetDamage();
+        // ensure the pixmap is updated when direct scanout ends
+        surfaceItem->destroyPixmap();
+    }
+    return ret;
+}
+
+void OutputLayer::notifyNoScanoutCandidate()
+{
+    if (m_scanoutCandidate) {
+        m_scanoutCandidate->setScanoutHint(nullptr, {});
+        m_scanoutCandidate = nullptr;
+    }
 }
 
 void OutputLayer::setPosition(const QPointF &position)

src/core/outputlayer.h
@@ -10,6 +10,7 @@
 #include "kwin_export.h"
 
 #include <QObject>
+#include <QPointer>
 #include <QRegion>
 #include <chrono>
 #include <optional>
@@ -18,6 +19,8 @@ namespace KWin
 {
 
 class SurfaceItem;
+class DrmDevice;
+class GraphicsBuffer;
 
 struct OutputLayerBeginFrameInfo
 {
@@ -69,20 +72,31 @@ public:
      * Tries to import the newest buffer of the surface for direct scanout
      * Returns @c true if scanout succeeds, @c false if rendering is necessary
      */
-    virtual bool scanout(SurfaceItem *surfaceItem);
+    bool attemptScanout(SurfaceItem *item);
+
+    /**
+     * Notify that there's no scanout candidate this frame
+     */
+    void notifyNoScanoutCandidate();
 
     /**
      * queries the render time of the last frame. If rendering isn't complete yet, this may block until it is
      */
     virtual std::chrono::nanoseconds queryRenderTime() const = 0;
 
-private:
+    virtual DrmDevice *scanoutDevice() const = 0;
+    virtual QHash<uint32_t, QList<uint64_t>> supportedDrmFormats() const = 0;
+
+protected:
+    virtual bool doAttemptScanout(GraphicsBuffer *buffer, const QRectF &sourceRect, const QSizeF &targetSize, OutputTransform transform, const ColorDescription &color, const QRegion &damage);
+
     QRegion m_repaints;
     QPointF m_hotspot;
     QPointF m_position;
     QSizeF m_size;
     qreal m_scale = 1.0;
     bool m_enabled = false;
+    QPointer<SurfaceItem> m_scanoutCandidate;
 };
 
 } // namespace KWin