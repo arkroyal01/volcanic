DawfukFR/kwin

backends: move render time queries into OutputFrame
This way, multiple OutputFrames can be pending at the same time, without
interfering in each other's render time queries

src/backends/drm/drm_virtual_egl_layer.cpp
@@ -71,9 +71,7 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglGbmLayer::beginFrame()
         m_scanoutBuffer = nullptr;
     }
 
-    if (!m_query) {
-        m_query = std::make_unique<GLRenderTimeQuery>();
-    }
+    m_query = std::make_unique<GLRenderTimeQuery>(m_eglBackend->openglContextRef());
     m_query->begin();
 
     const QRegion repair = m_damageJournal.accumulate(slot->age(), infiniteRegion());
@@ -83,9 +81,12 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglGbmLayer::beginFrame()
     };
 }
 
-bool VirtualEglGbmLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
+bool VirtualEglGbmLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
     m_query->end();
+    if (frame) {
+        frame->addRenderTimeQuery(std::move(m_query));
+    }
     glFlush();
     m_currentDamage = damagedRegion;
     m_damageJournal.add(damagedRegion);
@@ -182,11 +183,5 @@ void VirtualEglGbmLayer::releaseBuffers()
         m_scanoutBuffer->unref();
         m_scanoutBuffer = nullptr;
     }
-}
-
-std::chrono::nanoseconds VirtualEglGbmLayer::queryRenderTime() const
-{
-    m_eglBackend->makeCurrent();
-    return m_query->result();
 }
 }

src/backends/virtual/virtual_egl_backend.cpp
@@ -50,9 +50,7 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglLayer::beginFrame()
         return std::nullopt;
     }
 
-    if (!m_query) {
-        m_query = std::make_unique<GLRenderTimeQuery>();
-    }
+    m_query = std::make_unique<GLRenderTimeQuery>(m_backend->openglContextRef());
     m_query->begin();
 
     return OutputLayerBeginFrameInfo{
@@ -61,22 +59,15 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglLayer::beginFrame()
     };
 }
 
-bool VirtualEglLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
+bool VirtualEglLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
     m_query->end();
+    if (frame) {
+        frame->addRenderTimeQuery(std::move(m_query));
+    }
     glFlush(); // flush pending rendering commands.
     Q_EMIT m_output->outputChange(damagedRegion);
     return true;
-}
-
-std::chrono::nanoseconds VirtualEglLayer::queryRenderTime() const
-{
-    if (m_query) {
-        m_backend->makeCurrent();
-        return m_query->result();
-    } else {
-        return std::chrono::nanoseconds::zero();
-    }
 }
 
 VirtualEglBackend::VirtualEglBackend(VirtualBackend *b)

src/backends/x11/windowed/x11_windowed_qpainter_backend.cpp
@@ -47,16 +47,19 @@ std::optional<OutputLayerBeginFrameInfo> X11WindowedQPainterPrimaryLayer::beginF
     QRegion repaint = m_output->exposedArea() + m_output->rect();
     m_output->clearExposedArea();
 
-    m_renderStart = std::chrono::steady_clock::now();
+    m_renderTime = std::make_unique<CpuRenderTimeQuery>();
     return OutputLayerBeginFrameInfo{
         .renderTarget = RenderTarget(m_current->view()->image()),
         .repaint = repaint,
     };
 }
 
-bool X11WindowedQPainterPrimaryLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
+bool X11WindowedQPainterPrimaryLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
-    m_renderTime = std::chrono::steady_clock::now() - m_renderStart;
+    m_renderTime->end();
+    if (frame) {
+        frame->addRenderTimeQuery(std::move(m_renderTime));
+    }
     return true;
 }
 
@@ -90,11 +93,6 @@ void X11WindowedQPainterPrimaryLayer::present()
                        nullptr);
 }
 
-std::chrono::nanoseconds X11WindowedQPainterPrimaryLayer::queryRenderTime() const
-{
-    return m_renderTime;
-}
-
 X11WindowedQPainterCursorLayer::X11WindowedQPainterCursorLayer(X11WindowedOutput *output)
     : m_output(output)
 {
@@ -108,21 +106,19 @@ std::optional<OutputLayerBeginFrameInfo> X11WindowedQPainterCursorLayer::beginFr
         m_buffer = QImage(bufferSize, QImage::Format_ARGB32_Premultiplied);
     }
 
-    m_renderStart = std::chrono::steady_clock::now();
+    m_renderTime = std::make_unique<CpuRenderTimeQuery>();
     return OutputLayerBeginFrameInfo{
         .renderTarget = RenderTarget(&m_buffer),
         .repaint = infiniteRegion(),
     };
 }
 
-std::chrono::nanoseconds X11WindowedQPainterCursorLayer::queryRenderTime() const
+bool X11WindowedQPainterCursorLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
-    return m_renderTime;
-}
-
-bool X11WindowedQPainterCursorLayer::endFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
-{
-    m_renderTime = std::chrono::steady_clock::now() - m_renderStart;
+    m_renderTime->end();
+    if (frame) {
+        frame->addRenderTimeQuery(std::move(m_renderTime));
+    }
     m_output->cursor()->update(m_buffer, hotspot());
     return true;
 }

src/core/renderbackend.cpp
@@ -9,9 +9,26 @@
 #include "scene/surfaceitem.h"
 
 #include <drm_fourcc.h>
+#include <ranges>
 
 namespace KWin
 {
+
+CpuRenderTimeQuery::CpuRenderTimeQuery()
+    : m_start(std::chrono::steady_clock::now())
+{
+}
+
+void CpuRenderTimeQuery::end()
+{
+    m_end = std::chrono::steady_clock::now();
+}
+
+std::chrono::nanoseconds CpuRenderTimeQuery::queryRenderTime()
+{
+    Q_ASSERT(m_end);
+    return *m_end - m_start;
+}
 
 OutputFrame::OutputFrame(RenderLoop *loop)
     : m_loop(loop)
@@ -25,8 +42,12 @@ void OutputFrame::addFeedback(std::unique_ptr<PresentationFeedback> &&feedback)
     m_feedbacks.push_back(std::move(feedback));
 }
 
-void OutputFrame::presented(std::chrono::nanoseconds refreshDuration, std::chrono::nanoseconds timestamp, std::chrono::nanoseconds renderTime, PresentationMode mode)
+void OutputFrame::presented(std::chrono::nanoseconds refreshDuration, std::chrono::nanoseconds timestamp, PresentationMode mode)
 {
+    const auto view = m_renderTimeQueries | std::views::transform([](const auto &query) {
+                          return query->queryRenderTime();
+                      });
+    const auto renderTime = std::accumulate(view.begin(), view.end(), std::chrono::nanoseconds::zero());
     RenderLoopPrivate::get(m_loop)->notifyFrameCompleted(timestamp, renderTime, mode);
     for (const auto &feedback : m_feedbacks) {
         feedback->presented(refreshDuration, timestamp, mode);
@@ -56,6 +77,11 @@ void OutputFrame::setPresentationMode(PresentationMode mode)
 PresentationMode OutputFrame::presentationMode() const
 {
     return m_presentationMode;
+}
+
+void OutputFrame::addRenderTimeQuery(std::unique_ptr<RenderTimeQuery> &&query)
+{
+    m_renderTimeQueries.push_back(std::move(query));
 }
 
 RenderBackend::RenderBackend(QObject *parent)

arkroyal01/volcanic

src/backends/drm/drm_virtual_egl_layer.cpp
@@ -67,9 +67,7 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglGbmLayer::doBeginFrame()
 
     m_currentSlot = slot;
 
-    if (!m_query) {
-        m_query = std::make_unique<GLRenderTimeQuery>();
-    }
+    m_query = std::make_unique<GLRenderTimeQuery>(m_eglBackend->openglContextRef());
     m_query->begin();
 
     const QRegion repair = m_damageJournal.accumulate(slot->age(), infiniteRegion());
@@ -79,9 +77,10 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglGbmLayer::doBeginFrame()
     };
 }
 
-bool VirtualEglGbmLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
+bool VirtualEglGbmLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
     m_query->end();
+    frame->addRenderTimeQuery(std::move(m_query));
     glFlush();
     m_damageJournal.add(damagedRegion);
 
@@ -160,12 +159,6 @@ void VirtualEglGbmLayer::releaseBuffers()
         m_scanoutBuffer->unref();
         m_scanoutBuffer = nullptr;
     }
-}
-
-std::chrono::nanoseconds VirtualEglGbmLayer::queryRenderTime() const
-{
-    m_eglBackend->makeCurrent();
-    return m_query->result();
 }
 
 DrmDevice *VirtualEglGbmLayer::scanoutDevice() const

src/backends/virtual/virtual_egl_backend.cpp
@@ -51,9 +51,7 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglLayer::doBeginFrame()
         return std::nullopt;
     }
 
-    if (!m_query) {
-        m_query = std::make_unique<GLRenderTimeQuery>();
-    }
+    m_query = std::make_unique<GLRenderTimeQuery>(m_backend->openglContextRef());
     m_query->begin();
 
     return OutputLayerBeginFrameInfo{
@@ -62,21 +60,12 @@ std::optional<OutputLayerBeginFrameInfo> VirtualEglLayer::doBeginFrame()
     };
 }
 
-bool VirtualEglLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
+bool VirtualEglLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
     m_query->end();
+    frame->addRenderTimeQuery(std::move(m_query));
     glFlush(); // flush pending rendering commands.
     return true;
-}
-
-std::chrono::nanoseconds VirtualEglLayer::queryRenderTime() const
-{
-    if (m_query) {
-        m_backend->makeCurrent();
-        return m_query->result();
-    } else {
-        return std::chrono::nanoseconds::zero();
-    }
 }
 
 DrmDevice *VirtualEglLayer::scanoutDevice() const

src/backends/x11/windowed/x11_windowed_qpainter_backend.cpp
@@ -48,16 +48,17 @@ std::optional<OutputLayerBeginFrameInfo> X11WindowedQPainterPrimaryLayer::doBegi
     QRegion repaint = m_output->exposedArea() + m_output->rect();
     m_output->clearExposedArea();
 
-    m_renderStart = std::chrono::steady_clock::now();
+    m_renderTime = std::make_unique<CpuRenderTimeQuery>();
     return OutputLayerBeginFrameInfo{
         .renderTarget = RenderTarget(m_current->view()->image()),
         .repaint = repaint,
     };
 }
 
-bool X11WindowedQPainterPrimaryLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
+bool X11WindowedQPainterPrimaryLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
-    m_renderTime = std::chrono::steady_clock::now() - m_renderStart;
+    m_renderTime->end();
+    frame->addRenderTimeQuery(std::move(m_renderTime));
     return true;
 }
 
@@ -91,11 +92,6 @@ void X11WindowedQPainterPrimaryLayer::present()
                        nullptr);
 }
 
-std::chrono::nanoseconds X11WindowedQPainterPrimaryLayer::queryRenderTime() const
-{
-    return m_renderTime;
-}
-
 DrmDevice *X11WindowedQPainterPrimaryLayer::scanoutDevice() const
 {
     return m_backend->drmDevice();
@@ -120,21 +116,19 @@ std::optional<OutputLayerBeginFrameInfo> X11WindowedQPainterCursorLayer::doBegin
         m_buffer = QImage(bufferSize, QImage::Format_ARGB32_Premultiplied);
     }
 
-    m_renderStart = std::chrono::steady_clock::now();
+    m_renderTime = std::make_unique<CpuRenderTimeQuery>();
     return OutputLayerBeginFrameInfo{
         .renderTarget = RenderTarget(&m_buffer),
         .repaint = infiniteRegion(),
     };
 }
 
-std::chrono::nanoseconds X11WindowedQPainterCursorLayer::queryRenderTime() const
+bool X11WindowedQPainterCursorLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion, OutputFrame *frame)
 {
-    return m_renderTime;
-}
-
-bool X11WindowedQPainterCursorLayer::doEndFrame(const QRegion &renderedRegion, const QRegion &damagedRegion)
-{
-    m_renderTime = std::chrono::steady_clock::now() - m_renderStart;
+    m_renderTime->end();
+    if (frame) {
+        frame->addRenderTimeQuery(std::move(m_renderTime));
+    }
     m_output->cursor()->update(m_buffer, hotspot());
     return true;
 }

src/core/renderbackend.cpp
@@ -10,9 +10,26 @@
 #include "syncobjtimeline.h"
 
 #include <drm_fourcc.h>
+#include <ranges>
 
 namespace KWin
 {
+
+CpuRenderTimeQuery::CpuRenderTimeQuery()
+    : m_start(std::chrono::steady_clock::now())
+{
+}
+
+void CpuRenderTimeQuery::end()
+{
+    m_end = std::chrono::steady_clock::now();
+}
+
+std::chrono::nanoseconds CpuRenderTimeQuery::query()
+{
+    Q_ASSERT(m_end);
+    return *m_end - m_start;
+}
 
 OutputFrame::OutputFrame(RenderLoop *loop)
     : m_loop(loop)
@@ -26,8 +43,12 @@ void OutputFrame::addFeedback(std::unique_ptr<PresentationFeedback> &&feedback)
     m_feedbacks.push_back(std::move(feedback));
 }
 
-void OutputFrame::presented(std::chrono::nanoseconds refreshDuration, std::chrono::nanoseconds timestamp, std::chrono::nanoseconds renderTime, PresentationMode mode)
+void OutputFrame::presented(std::chrono::nanoseconds refreshDuration, std::chrono::nanoseconds timestamp, PresentationMode mode)
 {
+    const auto view = m_renderTimeQueries | std::views::transform([](const auto &query) {
+        return query->query();
+    });
+    const auto renderTime = std::accumulate(view.begin(), view.end(), std::chrono::nanoseconds::zero());
     RenderLoopPrivate::get(m_loop)->notifyFrameCompleted(timestamp, renderTime, mode);
     for (const auto &feedback : m_feedbacks) {
         feedback->presented(refreshDuration, timestamp, mode);
@@ -67,6 +88,11 @@ void OutputFrame::setDamage(const QRegion &region)
 QRegion OutputFrame::damage() const
 {
     return m_damage;
+}
+
+void OutputFrame::addRenderTimeQuery(std::unique_ptr<RenderTimeQuery> &&query)
+{
+    m_renderTimeQueries.push_back(std::move(query));
 }
 
 RenderBackend::RenderBackend(QObject *parent)