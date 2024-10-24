KDE/kwin

backends/drm: artificially create HDR headroom on SDR screens when showing HDR content

src/core/renderbackend.h
@@ -96,6 +96,9 @@ public:
     std::chrono::nanoseconds refreshDuration() const;
     std::chrono::nanoseconds predictedRenderTime() const;
 
+    std::optional<double> desiredHdrHeadroom() const;
+    void setDesiredHdrHeadroom(double edr);
+
 private:
     std::optional<RenderTimeSpan> queryRenderTime() const;
 
@@ -109,6 +112,7 @@ private:
     QRegion m_damage;
     std::vector<std::unique_ptr<RenderTimeQuery>> m_renderTimeQueries;
     bool m_presented = false;
+    std::optional<double> m_desiredHdrHeadroom;
 };
 
 /**

arkroyal01/volcanic

src/core/renderbackend.h
@@ -99,6 +99,9 @@ public:
     std::optional<double> brightness() const;
     void setBrightness(double brightness);
 
+    std::optional<double> artificialHdrHeadroom() const;
+    void setArtificialHdrHeadroom(double edr);
+
 private:
     std::optional<RenderTimeSpan> queryRenderTime() const;
 
@@ -113,6 +116,7 @@ private:
     std::vector<std::unique_ptr<RenderTimeQuery>> m_renderTimeQueries;
     bool m_presented = false;
     std::optional<double> m_brightness;
+    std::optional<double> m_artificialHdrHeadroom;
 };
 
 /**