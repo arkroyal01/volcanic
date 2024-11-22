DawfukFR/kwin

adjust to KDecoration changes: make nearly all geometry floating point

src/kcms/decoration/declarative-plugin/previewclient.cpp
@@ -102,17 +102,17 @@ void PreviewClient::setIcon(const QIcon &pixmap)
     Q_EMIT iconChanged(m_icon);
 }
 
-int PreviewClient::width() const
+double PreviewClient::width() const
 {
     return m_width;
 }
 
-int PreviewClient::height() const
+double PreviewClient::height() const
 {
     return m_height;
 }
 
-QSize PreviewClient::size() const
+QSizeF PreviewClient::size() const
 {
     return QSize(m_width, m_height);
 }

src/kcms/decoration/declarative-plugin/previewclient.h
@@ -71,9 +71,9 @@ public:
     bool isShaded() const override;
     bool providesContextHelp() const override;
 
-    int width() const override;
-    int height() const override;
-    QSize size() const override;
+    double width() const override;
+    double height() const override;
+    QSizeF size() const override;
     QPalette palette() const override;
     QColor color(ColorGroup group, ColorRole role) const override;
     Qt::Edges adjacentScreenEdges() const override;

src/plugins/blur/blur.cpp
@@ -385,7 +385,7 @@ QRegion BlurEffect::decorationBlurRegion(const EffectWindow *w) const
         return QRegion();
     }
 
-    QRegion decorationRegion = QRegion(w->decoration()->rect()) - w->decorationInnerRect().toRect();
+    QRegion decorationRegion = QRegion(w->decoration()->rect().toAlignedRect()) - w->decorationInnerRect().toRect();
     //! we return only blurred regions that belong to decoration region
     return decorationRegion.intersected(w->decoration()->blurRegion());
 }

src/plugins/kdecorations/aurorae/src/aurorae.cpp
@@ -328,11 +328,12 @@ bool Decoration::init()
     updateBorders();
     if (m_view) {
         auto resizeWindow = [this] {
-            QRect rect(QPoint(0, 0), size());
+            QRectF rect(QPoint(0, 0), size());
             if (m_padding && !client()->isMaximized()) {
                 rect = rect.adjusted(-m_padding->left(), -m_padding->top(), m_padding->right(), m_padding->bottom());
             }
-            m_view->setGeometry(rect);
+            // TODO what can we do about this?
+            m_view->setGeometry(rect.toAlignedRect());
             updateBlur();
         };
         connect(this, &Decoration::bordersChanged, this, resizeWindow);
@@ -382,7 +383,7 @@ void Decoration::updateBorders()
     updateExtendedBorders();
 }
 
-void Decoration::paint(QPainter *painter, const QRect &repaintRegion)
+void Decoration::paint(QPainter *painter, const QRectF &repaintRegion)
 {
     if (!m_view) {
         return;
@@ -391,7 +392,7 @@ void Decoration::paint(QPainter *painter, const QRect &repaintRegion)
     const QImage image = m_view->bufferAsImage();
     const qreal dpr = image.devicePixelRatioF();
 
-    QRect nativeContentRect = QRect(m_contentRect.topLeft() * dpr, m_contentRect.size() * dpr);
+    QRect nativeContentRect = QRect((m_contentRect.topLeft() * dpr).toPoint(), (m_contentRect.size() * dpr).toSize());
 
     painter->fillRect(rect(), Qt::transparent);
     painter->drawImage(rect(), image, nativeContentRect);
@@ -580,7 +581,7 @@ void Decoration::updateBuffer()
     if (buffer.isNull()) {
         return;
     }
-    m_contentRect = QRect(QPoint(0, 0), m_view->contentItem()->size().toSize());
+    m_contentRect = QRectF(QPointF(0, 0), m_view->contentItem()->size());
     if (m_padding && (m_padding->left() > 0 || m_padding->top() > 0 || m_padding->right() > 0 || m_padding->bottom() > 0) && !client()->isMaximized()) {
         m_contentRect = m_contentRect.adjusted(m_padding->left(), m_padding->top(), -m_padding->right(), -m_padding->bottom());
     }

src/window.cpp
@@ -2366,22 +2366,22 @@ bool Window::isActiveFullScreen() const
     return ac && (ac == this || !ac->isOnOutput(output()) || ac->allMainWindows().contains(const_cast<Window *>(this)));
 }
 
-int Window::borderBottom() const
+double Window::borderBottom() const
 {
     return isDecorated() ? decoration()->borderBottom() : 0;
 }
 
-int Window::borderLeft() const
+double Window::borderLeft() const
 {
     return isDecorated() ? decoration()->borderLeft() : 0;
 }
 
-int Window::borderRight() const
+double Window::borderRight() const
 {
     return isDecorated() ? decoration()->borderRight() : 0;
 }
 
-int Window::borderTop() const
+double Window::borderTop() const
 {
     return isDecorated() ? decoration()->borderTop() : 0;
 }
@@ -2678,8 +2678,8 @@ void Window::updateDecorationInputShape()
         return;
     }
 
-    const QMargins borders = decoration()->borders();
-    const QMargins resizeBorders = decoration()->resizeOnlyBorders();
+    const QMarginsF borders = decoration()->borders();
+    const QMarginsF resizeBorders = decoration()->resizeOnlyBorders();
 
     const QRectF innerRect = QRectF(QPointF(borderLeft(), borderTop()), decoratedClient()->size());
     const QRectF outerRect = innerRect + borders + resizeBorders;

src/window.h
@@ -1551,10 +1551,10 @@ protected:
 
     // geometry handling
     void checkOffscreenPosition(QRectF *geom, const QRectF &screenArea);
-    int borderLeft() const;
-    int borderRight() const;
-    int borderTop() const;
-    int borderBottom() const;
+    double borderLeft() const;
+    double borderRight() const;
+    double borderTop() const;
+    double borderBottom() const;
 
     void blockGeometryUpdates(bool block);
     void blockGeometryUpdates();

arkroyal01/volcanic

src/kcms/decoration/declarative-plugin/previewclient.cpp
@@ -102,17 +102,17 @@ void PreviewClient::setIcon(const QIcon &pixmap)
     Q_EMIT iconChanged(m_icon);
 }
 
-int PreviewClient::width() const
+qreal PreviewClient::width() const
 {
     return m_width;
 }
 
-int PreviewClient::height() const
+qreal PreviewClient::height() const
 {
     return m_height;
 }
 
-QSize PreviewClient::size() const
+QSizeF PreviewClient::size() const
 {
     return QSize(m_width, m_height);
 }

src/kcms/decoration/declarative-plugin/previewclient.h
@@ -71,9 +71,9 @@ public:
     bool isShaded() const override;
     bool providesContextHelp() const override;
 
-    int width() const override;
-    int height() const override;
-    QSize size() const override;
+    qreal width() const override;
+    qreal height() const override;
+    QSizeF size() const override;
     QPalette palette() const override;
     QColor color(ColorGroup group, ColorRole role) const override;
     Qt::Edges adjacentScreenEdges() const override;

src/plugins/blur/blur.cpp
@@ -389,7 +389,7 @@ QRegion BlurEffect::decorationBlurRegion(const EffectWindow *w) const
         return QRegion();
     }
 
-    QRegion decorationRegion = QRegion(w->decoration()->rect()) - w->contentsRect().toRect();
+    QRegion decorationRegion = QRegion(w->decoration()->rect().toAlignedRect()) - w->contentsRect().toRect();
     //! we return only blurred regions that belong to decoration region
     return decorationRegion.intersected(w->decoration()->blurRegion());
 }

src/plugins/kdecorations/aurorae/src/aurorae.cpp
@@ -327,11 +327,12 @@ bool Decoration::init()
     updateBorders();
     if (m_view) {
         auto resizeWindow = [this] {
-            QRect rect(QPoint(0, 0), size());
+            QRectF rect(QPoint(0, 0), size());
             if (m_padding && !window()->isMaximized()) {
                 rect = rect.adjusted(-m_padding->left(), -m_padding->top(), m_padding->right(), m_padding->bottom());
             }
-            m_view->setGeometry(rect);
+            // TODO what can we do about this?
+            m_view->setGeometry(rect.toRect());
             updateBlur();
         };
         connect(this, &Decoration::bordersChanged, this, resizeWindow);
@@ -381,7 +382,7 @@ void Decoration::updateBorders()
     updateExtendedBorders();
 }
 
-void Decoration::paint(QPainter *painter, const QRect &repaintRegion)
+void Decoration::paint(QPainter *painter, const QRectF &repaintRegion)
 {
     if (!m_view) {
         return;
@@ -390,7 +391,7 @@ void Decoration::paint(QPainter *painter, const QRect &repaintRegion)
     const QImage image = m_view->bufferAsImage();
     const qreal dpr = image.devicePixelRatioF();
 
-    QRect nativeContentRect = QRect(m_contentRect.topLeft() * dpr, m_contentRect.size() * dpr);
+    QRect nativeContentRect = QRect((m_contentRect.topLeft() * dpr).toPoint(), (m_contentRect.size() * dpr).toSize());
 
     painter->fillRect(rect(), Qt::transparent);
     painter->drawImage(rect(), image, nativeContentRect);
@@ -579,7 +580,7 @@ void Decoration::updateBuffer()
     if (buffer.isNull()) {
         return;
     }
-    m_contentRect = QRect(QPoint(0, 0), m_view->contentItem()->size().toSize());
+    m_contentRect = QRectF(QPoint(0, 0), m_view->contentItem()->size());
     if (m_padding && (m_padding->left() > 0 || m_padding->top() > 0 || m_padding->right() > 0 || m_padding->bottom() > 0) && !window()->isMaximized()) {
         m_contentRect = m_contentRect.adjusted(m_padding->left(), m_padding->top(), -m_padding->right(), -m_padding->bottom());
     }

src/window.cpp
@@ -2387,22 +2387,22 @@ bool Window::isActiveFullScreen() const
     return ac && (ac == this || !ac->isOnOutput(output()) || ac->allMainWindows().contains(const_cast<Window *>(this)));
 }
 
-int Window::borderBottom() const
+qreal Window::borderBottom() const
 {
     return isDecorated() ? decoration()->borderBottom() : 0;
 }
 
-int Window::borderLeft() const
+qreal Window::borderLeft() const
 {
     return isDecorated() ? decoration()->borderLeft() : 0;
 }
 
-int Window::borderRight() const
+qreal Window::borderRight() const
 {
     return isDecorated() ? decoration()->borderRight() : 0;
 }
 
-int Window::borderTop() const
+qreal Window::borderTop() const
 {
     return isDecorated() ? decoration()->borderTop() : 0;
 }
@@ -2711,8 +2711,8 @@ void Window::updateDecorationInputShape()
         return;
     }
 
-    const QMargins borders = decoration()->borders();
-    const QMargins resizeBorders = decoration()->resizeOnlyBorders();
+    const QMarginsF borders = decoration()->borders();
+    const QMarginsF resizeBorders = decoration()->resizeOnlyBorders();
 
     const QRectF innerRect = QRectF(QPointF(borderLeft(), borderTop()), decoratedWindow()->size());
     const QRectF outerRect = innerRect + borders + resizeBorders;

src/window.h
@@ -1586,10 +1586,10 @@ protected:
 
     // geometry handling
     void checkOffscreenPosition(QRectF *geom, const QRectF &screenArea);
-    int borderLeft() const;
-    int borderRight() const;
-    int borderTop() const;
-    int borderBottom() const;
+    qreal borderLeft() const;
+    qreal borderRight() const;
+    qreal borderTop() const;
+    qreal borderBottom() const;
 
     enum class MoveResizeMode : uint {
         None,