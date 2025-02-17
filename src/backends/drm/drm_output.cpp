arkroyal01/volcanic

commit 5334082d12f40e29e5a2bd371cbdd5900298b885

backends/drm: apply night light in the correct place with ICC profiles + KMS offloading
After the color ops for the ICC profile are added, the transfer function of the color description
doesn't make any sense anymore.

src/backends/drm/drm_output.cpp
@@ -593,14 +593,16 @@ void DrmOutput::tryKmsColorOffloading()
     if (m_state.colorProfileSource == ColorProfileSource::ICC && m_state.iccProfile) {
         colorPipeline.addTransferFunction(colorDescription().transferFunction());
         colorPipeline.addMultiplier(1.0 / colorDescription().referenceLuminance());
+        colorPipeline.addMultiplier(channelFactors);
         colorPipeline.add1DLUT(m_state.iccProfile->inverseTransferFunction());
         if (m_state.iccProfile->vcgt()) {
             colorPipeline.add1DLUT(m_state.iccProfile->vcgt());
         }
+    } else {
+        colorPipeline.addTransferFunction(colorDescription().transferFunction());
+        colorPipeline.addMultiplier(channelFactors);
+        colorPipeline.addInverseTransferFunction(colorDescription().transferFunction());
     }
-    colorPipeline.addTransferFunction(colorDescription().transferFunction());
-    colorPipeline.addMultiplier(channelFactors);
-    colorPipeline.addInverseTransferFunction(colorDescription().transferFunction());
     m_pipeline->setCrtcColorPipeline(colorPipeline);
     if (DrmPipeline::commitPipelines({m_pipeline}, DrmPipeline::CommitMode::Test) == DrmPipeline::Error::None) {
         m_pipeline->applyPendingChanges();

commit 02a4e2763108c13b5492657d9d773d06ff873ae4

backends/drm: correctly check if an ICC profile is used
The color profile source doesn't apply when HDR is enabled. This regressed in
5334082d12f40e29e5a2bd371cbdd5900298b885

src/backends/drm/drm_output.cpp
@@ -589,7 +589,7 @@ void DrmOutput::tryKmsColorOffloading()
     const double maxLuminance = colorDescription().maxHdrLuminance().value_or(colorDescription().referenceLuminance());
     const ColorDescription optimal = colorDescription().transferFunction().type == TransferFunction::gamma22 ? colorDescription() : colorDescription().withTransferFunction(TransferFunction(TransferFunction::gamma22, 0, maxLuminance));
     ColorPipeline colorPipeline = ColorPipeline::create(optimal, colorDescription(), RenderingIntent::RelativeColorimetric);
-    if (m_state.colorProfileSource == ColorProfileSource::ICC && m_state.iccProfile) {
+    if (usesICC) {
         colorPipeline.addTransferFunction(colorDescription().transferFunction());
         colorPipeline.addMultiplier(1.0 / colorDescription().referenceLuminance());
         colorPipeline.addMultiplier(channelFactors);