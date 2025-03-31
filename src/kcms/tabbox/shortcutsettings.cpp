josephcrowell/kwin-x11-improved

tabbox: Add switching shortcuts that include Meta key
The long term goal is to use Meta+Tab only for window switching. As the
first step, this change reserves Meta+Tab for window switching. Alt+Tab
is still operational. In other words, both Alt+Tab and Meta+Tab can be
used for window switching.

src/kcms/tabbox/shortcutsettings.cpp
@@ -128,10 +128,10 @@ ShortcutSettings::ShortcutSettings(QObject *parent)
     };
 
     // TabboxType::Main
-    addShortcut(ki18nd("kwin", "Walk Through Windows"), {Qt::ALT | Qt::Key_Tab});
-    addShortcut(ki18nd("kwin", "Walk Through Windows (Reverse)"), {Qt::ALT | Qt::SHIFT | Qt::Key_Tab});
-    addShortcut(ki18nd("kwin", "Walk Through Windows of Current Application"), {Qt::ALT | Qt::Key_QuoteLeft});
-    addShortcut(ki18nd("kwin", "Walk Through Windows of Current Application (Reverse)"), {Qt::ALT | Qt::Key_AsciiTilde});
+    addShortcut(ki18nd("kwin", "Walk Through Windows"), {Qt::META | Qt::Key_Tab, Qt::ALT | Qt::Key_Tab});
+    addShortcut(ki18nd("kwin", "Walk Through Windows (Reverse)"), {Qt::META | Qt::SHIFT | Qt::Key_Tab, Qt::ALT | Qt::SHIFT | Qt::Key_Tab});
+    addShortcut(ki18nd("kwin", "Walk Through Windows of Current Application"), {Qt::META | Qt::Key_QuoteLeft, Qt::ALT | Qt::Key_QuoteLeft});
+    addShortcut(ki18nd("kwin", "Walk Through Windows of Current Application (Reverse)"), {Qt::META | Qt::Key_AsciiTilde, Qt::ALT | Qt::Key_AsciiTilde});
     // TabboxType::Alternative
     addShortcut(ki18nd("kwin", "Walk Through Windows Alternative"));
     addShortcut(ki18nd("kwin", "Walk Through Windows Alternative (Reverse)"));

arkroyal01/volcanic

src/kcms/tabbox/shortcutsettings.cpp
@@ -128,10 +128,10 @@ ShortcutSettings::ShortcutSettings(QObject *parent)
     };
 
     // TabboxType::Main
-    addShortcut(ki18nd("kwin_x11", "Walk Through Windows"), {Qt::ALT | Qt::Key_Tab});
-    addShortcut(ki18nd("kwin_x11", "Walk Through Windows (Reverse)"), {Qt::ALT | Qt::SHIFT | Qt::Key_Tab});
-    addShortcut(ki18nd("kwin_x11", "Walk Through Windows of Current Application"), {Qt::ALT | Qt::Key_QuoteLeft});
-    addShortcut(ki18nd("kwin_x11", "Walk Through Windows of Current Application (Reverse)"), {Qt::ALT | Qt::Key_AsciiTilde});
+    addShortcut(ki18nd("kwin_x11", "Walk Through Windows"), {Qt::META | Qt::Key_Tab, Qt::ALT | Qt::Key_Tab});
+    addShortcut(ki18nd("kwin_x11", "Walk Through Windows (Reverse)"), {Qt::META | Qt::SHIFT | Qt::Key_Tab, Qt::ALT | Qt::SHIFT | Qt::Key_Tab});
+    addShortcut(ki18nd("kwin_x11", "Walk Through Windows of Current Application"), {Qt::META | Qt::Key_QuoteLeft, Qt::ALT | Qt::Key_QuoteLeft});
+    addShortcut(ki18nd("kwin_x11", "Walk Through Windows of Current Application (Reverse)"), {Qt::META | Qt::Key_AsciiTilde, Qt::ALT | Qt::Key_AsciiTilde});
     // TabboxType::Alternative
     addShortcut(ki18nd("kwin_x11", "Walk Through Windows Alternative"));
     addShortcut(ki18nd("kwin_x11", "Walk Through Windows Alternative (Reverse)"));